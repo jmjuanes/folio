49a6d19447d67790d75694f19c3bddfcf34dcada
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-05-27 16:51:42 +0000

    feat: init toasts component

---
+++ gitdraw-react/components/Toasts.js
@@ -0,0 +1,53 @@
+import React from "react";
+import kofi from "kofi";
+
+// Toast child
+const Toast = props => {
+    const timer = React.useRef(null);
+    React.useEffect(() => {
+        if (props.autoDelete && props.autoDeleteTime > 0) {
+            timer.current = setTimeout(() => props.onDelete(), props.autoDeleteTime);
+        }
+    }, []);
+
+    const classList = kofi.classNames({
+        "alert is-flex has-mb-0 has-mt-2": true,
+        "has-bg-red-500": props.type === "error",
+        "has-bg-yellow-500": props.type === "warning",
+        "has-bg-green-500": props.type === "success",
+    });
+
+    return (
+        <div className={classList}>
+            <div className="has-mr-4">{props.message}</div>
+            <div
+                className="close has-ml-auto"
+                onClick={() => {
+                    clearTimeout(timer.current);
+                    props.onDelete();
+                }}
+            />
+        </div>
+    );
+};
+
+export const Toasts = props => (
+    <div className="is-absolute has-bottom-none has-right-none has-mr-4 has-mb-4 has-w-96">
+        {props.items.map(item => (
+            <Toast
+                key={item.id}
+                type={item.type}
+                message={item.message}
+                autoDelete={props.autoDelete}
+                autoDeleteTime={props.autoDeleteTime}
+                onDelete={() => props.onDelete(item.id)}
+            />
+        ))}
+    </div>
+);
+
+Toasts.defaultProps = {
+    items: [],
+    autoDelete: true,
+    autoDeleteTime: 6000,
+};
