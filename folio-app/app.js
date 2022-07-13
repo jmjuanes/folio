bd2ee42b376d878831ef5a4b0fd7e2b0fe550af9
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-07-13 17:09:58 +0000

    fix: fixed main app component to copy screenshots to clipboard

--- folio-app/app.js
+++ folio-app/app.js
@@ -1,12 +1,27 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-
 import {Folio} from "folio-react";
+import {blobToClipboard} from "folio-react/utils/blob.js";
+
+import {Toasts} from "./components/Toasts.js";
+import {useNotifications} from "./hooks/useNotifications.js";
 
 const App = props => {
+    const ref = React.useRef(null);
+    const notifications = useNotifications();
     return (
         <div className="is-fixed has-top-none has-left-none has-w-full has-h-full">
-            <Folio />
+            <Folio
+                ref={ref}
+                onScreenshot={blob => {
+                    blobToClipboard(blob);
+                    notifications.success("Screenshot copied to clipboard");
+                }}
+            />
+            <Toasts
+                items={notifications.getAll()}
+                onDelete={id => notifications.remove(id)}
+            />
         </div>
     );
 };
