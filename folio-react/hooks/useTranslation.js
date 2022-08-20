7719259da1cf0b31634b09537e528662adb7bc6d
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-08-20 16:53:02 +0000

    feat: init translations

---
+++ folio-react/hooks/useTranslation.js
@@ -0,0 +1,12 @@
+import defaultLanguage from "../locales/en.json";
+
+const getInObject = (obj, path) => {
+    return path.split(".").reduce((o, k) => o?.[k] || "", obj);
+};
+
+export const useTranslation = () => {
+    //TODO: allow to change the default language
+    return {
+        t: path => getInObject(defaultLanguage, path) || "",
+    };
+};
