8f5fec1c3019c7da36ed8f041fe894076a389355
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-08-21 18:01:21 +0000

    feat: improve blob utilities

--- folio-react/utils/blob.js
+++ folio-react/utils/blob.js
@@ -1,19 +1,28 @@
-// Save Blob to file
-// Based on https://stackoverflow.com/a/19328891
-export const blobToFile = (blob, name) => {
-    // Create the link element to download the file
-    const link = document.createElement("a");
-    link.style.display = "none"; // Hide link element
-    document.body.appendChild(link); // Append to body
-    // let blob = new Blob([content], {
-    //     "type": type
-    // });
-    const url = window.URL.createObjectURL(blob); // Create url
-    link.href = url; // Set the link url as the generated url
-    link.download = name; // Set the filename
-    link.click(); // Download the file
-    window.URL.revokeObjectURL(url); // Revoke url
-    document.body.removeChild(link); // Remove from body
+// Create a new blob object
+export const createBlob = (content, type) => {
+    return new Blob([content], {type: type});
+};
+
+// Create a blob from a file object
+export const blobFromFile = (file, type) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            const blob = new Blob([new Uint8Array(reader.result)], {
+                type: type || file.type,
+            });
+            resolve(blob);
+        });
+        reader.addEventListener("error", e => reject(e));
+        reader.readAsArrayBuffer(file);
+    });
+};
+
+// Convert a blob to file
+export const blobToFile = (blob, name, type) => {
+    return new Promise(resolve => {
+        return resolve(new File([blob], name, {type: type || blob.type}));
+    });
 };
 
 // Save Blob to clopboard
