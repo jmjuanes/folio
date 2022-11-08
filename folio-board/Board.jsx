6ff71fd655f7b7802f4727b74ce856284693f557
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-11-08 19:18:41 +0000

    feat: rewrite board component for using SVG renderer

---
+++ folio-board/Board.jsx
@@ -0,0 +1,377 @@
+import React from "react";
+import {
+    normalizeRectangle,
+    pointInRectangle,
+} from "folio-math";
+import {
+    Renderer,
+    HANDLERS,
+    defaultTools,
+} from "folio-renderer";
+
+import {
+    IS_DARWIN,
+    ACTIONS,
+    EVENTS,
+    KEYS,
+    ZOOM_STEP,
+} from "./constants.js";
+import {useBoard} from "./hooks/useBoard.js";
+import {
+    EditionPanel,
+    HistoryPanel,
+    MenuPanel,
+    ToolsPanel,
+    ZoomPanel,
+} from "./components/Panels/index.jsx";
+import {generateID, isArrowKey, isInputTarget} from "./utils/index.js";
+
+export const Board = props => {
+    const [updateKey, forceUpdate] = React.useReducer(x => x + 1, 0);
+    const board = useBoard([]);
+    const state = React.useRef({
+        action: null,
+        activeTool: null,
+        activeElement: null,
+        activeHandler: null,
+        selectionCount: 0,
+        brush: null,
+        snapshot: null,
+        zoom: 1,
+    });
+
+    // Handle canvas point --> reset current selection
+    const handlePointCanvas = () => {
+        if (!state.current.activeTool) {
+            board.current.clearSelectedElements();
+            forceUpdate();
+        }
+    };
+
+    // Handle point element
+    const handlePointElement = event => {
+        if (!state.current.activeTool) {
+            const element = board.current.getElementById(event.element);
+            if (!event.shiftKey) {
+                board.current.clearSelectedElements();
+                element.selected = true;
+            }
+            else {
+                // Toggle element selection
+                element.selected = !element.selected;
+            }
+            forceUpdate();
+        }
+    };
+
+    // Handle point handler
+    const handlePointHandler = event => {
+        state.current.action = ACTIONS.RESIZE_ELEMENT,
+        state.current.activeHandler = event.handler;
+    };
+
+    const handlePointerDown = event => {
+        if (state.current.activeTool) {
+            state.current.action = ACTIONS.CREATE_ELEMENT;
+            const element = {
+                ...props.tools[state.current.activeTool]?.onCreateStart?.({}, event, {}),
+                id: generateID(),
+                type: state.current.activeTool,
+                x: event.originalX,
+                y: event.originalY,
+                selected: false,
+                locked: false,
+            };
+            state.current.activeElement = element; // Save element reference
+            board.current.addElement(element);
+            // board.current.activeGroup = null; // Reset current group
+            board.current.clearSelectedElements();
+        }
+        else if (board.current.getSelectedElements().length > 0) {
+            if (!state.current.action) {
+                state.current.action = ACTIONS.DRAG_ELEMENT;
+            }
+            state.current.snapshot = board.current.snapshotSelectedElements();
+        }
+        else if (!state.current.action) {
+            state.current.action = ACTIONS.SELECTION;
+            state.current.brush = {
+                x: event.originalX,
+                y: event.originalY,
+                width: 0,
+                height: 0,
+            };
+        }
+        forceUpdate();
+    };
+
+    const handlePointerMove = event => {
+        if (state.current.action === ACTIONS.CREATE_ELEMENT) {
+            Object.assign(
+                state.current.activeElement,
+                props.tools[state.current.activeTool]?.onCreateMove?.(state.current.activeElement, event),
+            );
+        }
+        else if (state.current.action === ACTIONS.DRAG_ELEMENT) {
+            const snapshot = state.current.snapshot;
+            board.current.getSelectedElements().forEach((element, index) => {
+                Object.assign(element, props.tools[element.type]?.onDrag?.(snapshot[index], event) || {});
+            });
+        }
+        else if (state.current.action === ACTIONS.RESIZE_ELEMENT) {
+            const snapshot = state.current.snapshot[0];
+            const element = board.current.getElementById(snapshot.id);
+            if (state.current.activeHandler === HANDLERS.CORNER_TOP_LEFT) {
+                element.x = Math.min(snapshot.x + event.dx, snapshot.x + snapshot.width - 1); // getPosition(snapshot.x + x);
+                element.width = snapshot.width + (snapshot.x - element.x);
+                element.y = Math.min(snapshot.y + event.dy, snapshot.y + snapshot.height - 1); // getPosition(snapshot.y + y);
+                element.height = snapshot.height + (snapshot.y - element.y);
+            }
+            else if (state.current.activeHandler === HANDLERS.CORNER_TOP_RIGHT) {
+                element.width = Math.max(snapshot.width + event.dx, 1) // getPosition(element.x + snapshot.width + x) - element.x;
+                element.y = Math.min(snapshot.y + event.dy, snapshot.y + snapshot.height - 1); // getPosition(snapshot.y + y);
+                element.height = snapshot.height + (snapshot.y - element.y);
+            }
+            else if (state.current.activeHandler === HANDLERS.CORNER_BOTTOM_LEFT) {
+                element.x = Math.min(snapshot.x + event.dx, snapshot.x + snapshot.width - 1); // getPosition(snapshot.x + x);
+                element.width = snapshot.width + (snapshot.x - element.x);
+                element.height = Math.max(snapshot.height + event.dy, 1);
+            }
+            else if (state.current.activeHandler === HANDLERS.CORNER_BOTTOM_RIGHT) {
+                element.width = Math.max(snapshot.width + event.dx, 1); // getPosition(element.x + snapshot.width + x) - element.x;
+                element.height = Math.max(snapshot.height + event.dy, 1);
+            }
+            else if (state.current.activeHandler === HANDLERS.EDGE_TOP) {
+                element.y = Math.min(snapshot.y + event.dy, snapshot.y + snapshot.height - 1);
+                element.height = snapshot.height + (snapshot.y - element.y);
+            }
+            else if (state.current.activeHandler === HANDLERS.EDGE_BOTTOM) {
+                element.height = Math.max(snapshot.height + event.dy, 1);
+            }
+            else if (state.current.activeHandler === HANDLERS.EDGE_LEFT) {
+                element.x = Math.min(snapshot.x + event.dx, snapshot.x + snapshot.width - 1);
+                element.width = snapshot.width + (snapshot.x - element.x);
+            }
+            else if (state.current.activeHandler === HANDLERS.EDGE_RIGHT) {
+                element.width = Math.max(snapshot.width + event.dx, 1);
+            }
+        }
+        else if (state.current.action === ACTIONS.SELECTION || state.current.action === ACTIONS.SCREENSHOT) {
+            state.current.brush.width = event.dx;
+            state.current.brush.height = event.dy;
+        }
+        forceUpdate();
+    };
+
+    const handlePointerUp = () => {
+        if (state.current.action === ACTIONS.CREATE_ELEMENT) {
+            state.current.activeElement.selected = true;
+            state.current.activeElement = null;
+            state.current.activeTool = null; // reset active tool
+        }
+        else if (state.current.action === ACTIONS.SELECTION) {
+            const rectangle = normalizeRectangle(state.current.brush);
+            // Select all elements that are in the selected rectangle
+            board.current.getElements().forEach(element => {
+                const points = props.tools[element.type]?.getBoundaryPoints(element) || [];
+                element.selected = points.some(point => {
+                    return pointInRectangle(point, rectangle);
+                });
+            });
+        }
+        // Reset current state
+        state.current.action = null;
+        state.current.brush = null;
+        forceUpdate();
+    };
+
+    // Key down listener
+    React.useEffect(() => {
+        const handleKeyDown = event => {
+            // TODO: check text input
+            const isCtrlKey = IS_DARWIN ? event.metaKey : event.ctrlKey;
+            if (event.key === KEYS.BACKSPACE || (isCtrlKey && (event.key === KEYS.C || event.key === KEYS.X))) {
+                event.preventDefault();
+                if (event.key === KEYS.X || event.key === KEYS.C) {
+                    const elements = board.current.copySelectedElements();
+                    // copyTextToClipboard(`folio:::${JSON.stringify(elements)}`);
+                }
+                // Check for backspace key or cut --> remove elements
+                if (event.key === KEYS.BACKSPACE || event.key === KEYS.X) {
+                    board.current.registerSelectionRemove();
+                    board.current.removeSelectedElements();
+                    // Reset active group if all elements of this group have been removed
+                    // if (board.current.getElementsInActiveGroup().length < 1) {
+                    //     board.current.activeGroup = null;
+                    // }
+                }
+                forceUpdate();
+            }
+            // Undo or redo key
+            else if (isCtrlKey && (event.key === KEYS.Z || event.key === KEYS.Y)) {
+                event.key === KEYS.Z ? board.current.undo() : board.current.redo();
+                forceUpdate();
+            }
+            // Check ESCAPE key
+            else if (event.key === KEYS.ESCAPE) {
+                event.preventDefault();
+                // Check if screenshot dialog is visible
+                // if (state.showExportDialog) {
+                //     return setState(prevState => ({...prevState, showExportDialog: false}));
+                // }
+                // // Check if we are in the screenshot mode
+                // else if (state.mode === MODES.SCREENSHOT) {
+                //     return setState(prevState => ({
+                //         ...prevState,
+                //         mode: MODES.SELECTION,
+                //     }));
+                // }
+                board.current.clearSelectedElements();
+                // board.current.activeGroup = null;
+                forceUpdate();
+            }
+            // Check for arrow keys --> move elements
+            else if (isArrowKey(event.key)) {
+                event.preventDefault();
+                // const step = state.gridEnabled ? state.gridSize : (event.shiftKey ? 5 : 1);
+                // const direction = (event.key === KEYS.ARROW_UP || event.key === KEYS.ARROW_DOWN) ? "y" : "x";
+                // const sign = (event.key === KEYS.ARROW_DOWN || event.key === KEYS.ARROW_RIGHT) ? +1 : -1;
+                // const selectedElements = board.current.getSelectedElements();
+                // board.current.addHistoryEntry({
+                //     type: ELEMENT_CHANGE_TYPES.UPDATE,
+                //     ids: selectedElements.map(el => el.id).join(","),
+                //     keys: direction,
+                //     elements: selectedElements.map(el => {
+                //         const prevValue = el[direction];
+                //         el[direction] = prevValue + step * sign;
+                //         return {
+                //             id: el.id,
+                //             prevValues: {[direction]: prevValue},
+                //             newValues: {[direction]: el[direction]},
+                //         };
+                //     }),
+                // });
+                forceUpdate();
+            }
+        };
+
+        document.addEventListener(EVENTS.KEY_DOWN, handleKeyDown);
+
+        // When the board is unmounted, remove keydown event listener
+        return () => {
+            document.removeEventListener(EVENTS.KEY_DOWN, handleKeyDown);
+        };
+    }, []);
+
+    const handleExportClick = () => {
+        return null;
+    };
+
+    const handleSaveClick = () => {
+        return null;
+    };
+
+    const {action, activeTool} = state.current;
+
+    return (
+        <div className="position-fixed overflow-hidden top-0 left-0 h-full w-full">
+            <Renderer
+                tools={props.tools}
+                elements={board.current.elements}
+                brush={state.current.brush}
+                onPointCanvas={handlePointCanvas}
+                onPointElement={handlePointElement}
+                onPointHandler={handlePointHandler}
+                onPointerDown={handlePointerDown}
+                onPointerMove={handlePointerMove}
+                onPointerUp={handlePointerUp}
+                showHandlers={!action && !activeTool}
+                showBrush={action === ACTIONS.SELECTION || action === ACTIONS.SCREENSHOT}
+                showSelection={!action && !activeTool}
+            />
+            <MenuPanel
+                onCameraClick={() => {
+                    // if (state.mode === MODES.SCREENSHOT) {
+                    //     return setState(prevState => ({
+                    //         ...prevState,
+                    //         mode: MODES.SELECTION,
+                    //     }));
+                    // }
+                    // return setState(prevState => ({
+                    //     ...prevState,
+                    //     showSreenshotDialog: true,
+                    // }));
+                }}
+                onExportClick={() => handleExportClick()}
+                onSaveClick={() => handleSaveClick()}
+            />
+            {state.current.action !== ACTIONS.SCREENSHOT && (
+                <React.Fragment>
+                    <ToolsPanel
+                        activeAction={state.current.action}
+                        activeTool={state.current.activeTool}
+                        onToolChange={tool => {
+                            state.current.activeTool = tool;
+                            board.current.clearSelectedElements();
+                            return forceUpdate();
+                            // setState(prevState => ({
+                            //     ...prevState,
+                            //     elementType: type,
+                            //     mode: MODES.NONE,
+                            // }));
+                        }}
+                    />
+                    <HistoryPanel
+                        undoDisabled={board.current.isUndoDisabled()}
+                        redoDisabled={board.current.isRedoDisabled()}
+                        onUndoClick={() => {
+                            board.current.undo();
+                            forceUpdate();
+                        }}
+                        onRedoClick={() => {
+                            board.current.redo();
+                            forceUpdate();
+                        }}
+                    />
+                    <ZoomPanel
+                        zoom={state.current.zoom}
+                        onZoomInClick={() => handleZoomChange(ZOOM_STEP)}
+                        onZoomOutClick={() => handleZoomChange(-ZOOM_STEP)}
+                    />
+                    <EditionPanel
+                        key={updateKey}
+                        selection={board.current.getSelectedElements()}
+                        styleDialogActive={!!state.current.showStyleDialog}
+                        onRemoveClick={() => {
+                            board.current.registerSelectionRemove();
+                            board.current.removeSelectedElements();
+                            forceUpdate();
+                        }}
+                        onBringForwardClick={() => {
+                            // boardApi.current.bringSelectionForward();
+                        }}
+                        onSendBackwardClick={() => {
+                            // boardApi.current.sendSelectionBackward();
+                        }}
+                        onGroupSelectionClick={() => {
+                            // const group = Folio.generateID();
+                            // board.current.registerSelectionUpdate(["group"], [group], false);
+                            // board.current.updateSelectedElements("group", group);
+                            // forceUpdate();
+                        }}
+                        onUngroupSelectionClick={() => {
+                            // board.current.registerSelectionUpdate(["group"], [null], false);
+                            // board.current.updateSelectedElements("group", null);
+                            // forceUpdate();
+                        }}
+                    />
+                </React.Fragment>
+            )}
+        </div>
+    );
+};
+
+Board.defaultProps = {
+    tools: defaultTools,
+    onScreenshot: null,
+};
