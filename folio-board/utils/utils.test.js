4c5e550442683b046267f78bc72b9e895940f821
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-11-18 19:08:15 +0000

    test: fix tests and removed unused

--- folio-board/utils/utils.test.js
+++
@@ -1,7 +0,0 @@
-import * as utils from "./index.js";
-
-describe("utils.generateID", () => {
-    it("should generate an ID string", () => {
-        expect(typeof utils.generateID()).toBe("string");
-    });
-});
