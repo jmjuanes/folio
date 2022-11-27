85b3a81e206975d09beee8b0b3655dad6cffcda5
Josemi <josemijuanes@gmail.com>
2022-11-27 16:45:51 +0100

    refactor: rename main index.jsx file in folio pkg

--- folio/index.js
+++
@@ -1,5 +0,0 @@
-// Export components
-export {Board} from "./Board.jsx";
-
-// Export hooks
-export * from "./hooks/useBoard.js";
---
+++ folio/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import {BoardProvider} from "./contexts/BoardContext.jsx";
+import {Board} from "./components/Board/index.jsx";
+
+const Folio = props => {
+    return (
+        <Board />
+    );
+};
+
+export default Folio;
