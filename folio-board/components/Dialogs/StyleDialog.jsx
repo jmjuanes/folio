d02f9295c3ce98af9b567331804c59099e0f3ef1
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-12-06 17:56:43 +0100

    feat: using new style properties in StyleDialog component

--- folio-board/components/Dialogs/StyleDialog.jsx
+++ folio-board/components/Dialogs/StyleDialog.jsx
@@ -2,51 +2,32 @@ import React from "react";
 import classNames from "classnames";
 
 import {
-    COLORS,
-    FONTS,
-    OPACITY_NONE,
-    OPACITY_SEMITRANSPARENT,
-    OPACITY_TRANSPARENT,
-    STROKE_SIZE_NONE,
-    STROKE_SIZE_SM,
-    STROKE_SIZE_MD,
-    STROKE_SIZE_LG,
-    STROKE_SIZE_XL,
-    RADIUS_NONE,
-    RADIUS_MD,
-    RADIUS_SM,
-    RADIUS_LG,
-    RADIUS_XL,
-    STROKE_STYLE_SOLID,
-    STROKE_STYLE_DOTTED,
-    STROKE_STYLE_DASHED,
-    FONT_SIZE_SM,
-    FONT_SIZE_MD,
-    FONT_SIZE_LG,
-    FONT_SIZE_XL,
+    COLOR_KEYS,
+    FONT_KEYS,
+    OPACITY_KEYS,
+    SIZE_KEYS,
+    DASH_KEYS,
 } from "../../constants.js";
-
+import {
+    fillColors,
+    strokeColors,
+    strokeWidths,
+    fontFaces,
+    fontSizes,
+} from "../../styles.js";
 import {Dialog} from "./Dialog.jsx";
 import {Form} from "../Form/index.jsx";
 import {
     TextIcon,
     FillIcon,
     StrokeIcon,
-    CornersIcon,
     OpacityNoneIcon,
     OpacitySemiTransparentIcon,
     OpacityTransparentIcon,
-    BanIcon,
     CircleSolidIcon,
     CircleDashedIcon,
     CircleDottedIcon,
 } from "../icons/index.jsx";
-
-const opacityValues = [
-    {value: OPACITY_NONE, icon: OpacityNoneIcon()},
-    {value: OPACITY_SEMITRANSPARENT, icon: OpacitySemiTransparentIcon()},
-    {value: OPACITY_TRANSPARENT, icon: OpacityTransparentIcon()},
-];
 
 const options = {
     fill: {
@@ -56,12 +37,19 @@ const options = {
             fillColor: {
                 type: "color",
                 title: "Fill Color",
-                values: COLORS,
+                values: Object.keys(COLOR_KEYS).map(key => ({
+                    value: key,
+                    color: fillColors[key],
+                })),
             },
             fillOpacity: {
                 type: "select",
                 title: "Fill Opacity",
-                values: opacityValues,
+                values: [
+                    {value: OPACITY_KEYS.NONE, icon: OpacityNoneIcon()},
+                    {value: OPACITY_KEYS.SEMITRANSPARENT, icon: OpacitySemiTransparentIcon()},
+                    {value: OPACITY_KEYS.TRANSPARENT, icon: OpacityTransparentIcon()},
+                ],
             },
         },
     },
@@ -72,48 +60,37 @@ const options = {
             strokeColor: {
                 type: "color",
                 title: "Stroke color",
-                values: COLORS,
+                values: Object.keys(COLOR_KEYS).map(key => ({
+                    value: key,
+                    color: strokeColors[key],
+                })),
             },
             strokeWidth: {
                 type: "select",
                 title: "Stroke Width",
                 values: [
-                    {value: STROKE_SIZE_NONE, icon: BanIcon()},
-                    {value: STROKE_SIZE_SM, text: "S"},
-                    {value: STROKE_SIZE_MD, text: "M"},
-                    {value: STROKE_SIZE_LG, text: "L"},
-                    {value: STROKE_SIZE_XL, text: "XL"},
+                    {value: SIZE_KEYS.SMALL, text: "S"},
+                    {value: SIZE_KEYS.MEDIUM, text: "M"},
+                    {value: SIZE_KEYS.LARGE, text: "L"},
+                    {value: SIZE_KEYS.XLARGE, text: "XL"},
                 ],
             },
             strokeStyle: {
                 type: "select",
                 title: "Stroke Style",
                 values: [
-                    {value: STROKE_STYLE_SOLID, icon: CircleSolidIcon()},
-                    {value: STROKE_STYLE_DASHED, icon: CircleDashedIcon()},
-                    {value: STROKE_STYLE_DOTTED, icon: CircleDottedIcon()},
+                    {value: DASH_KEYS.SOLID, icon: CircleSolidIcon()},
+                    {value: DASH_KEYS.DASHED, icon: CircleDashedIcon()},
+                    {value: DASH_KEYS.DOTTED, icon: CircleDottedIcon()},
                 ],
             },
             strokeOpacity: {
                 type: "select",
                 title: "Stroke Opacity",
-                values: opacityValues,
-            },
-        },
-    },
-    radius: {
-        icon: CornersIcon(),
-        test: "radius",
-        fields: {
-            radius: {
-                type: "select",
-                title: "Corners",
                 values: [
-                    {value: RADIUS_NONE, icon: BanIcon()},
-                    {value: RADIUS_SM, text: "S"},
-                    {value: RADIUS_MD, text: "M"},
-                    {value: RADIUS_LG, text: "L"},
-                    {value: RADIUS_XL, text: "XL"},
+                    {value: OPACITY_KEYS.NONE, icon: OpacityNoneIcon()},
+                    {value: OPACITY_KEYS.SEMITRANSPARENT, icon: OpacitySemiTransparentIcon()},
+                    {value: OPACITY_KEYS.TRANSPARENT, icon: OpacityTransparentIcon()},
                 ],
             },
         },
@@ -125,68 +102,34 @@ const options = {
             textColor: {
                 type: "color",
                 title: "Text Color",
-                values: COLORS,
+                values: Object.keys(COLOR_KEYS).map(key => ({
+                    value: key,
+                    // color: textColors[key],
+                    color: strokeColors[key],
+                })),
             },
             textFont: {
                 type: "font",
                 title: "Text Font",
-                values: FONTS,
+                values: [
+                    {value: FONT_KEYS.SANS, font: fontFaces[FONT_KEYS.SANS]},
+                    {value: FONT_KEYS.SERIF, font: fontFaces[FONT_KEYS.SERIF]},
+                    // {value: FONT_KEYS.SANS, font: fontFaces[FONT_KEYS.SANS]},
+                    {value: FONT_KEYS.MONO, font: fontFaces[FONT_KEYS.MONO]},
+                ],
             },
             textSize: {
                 type: "select",
                 title: "Text Size",
                 values: [
-                    {value: FONT_SIZE_SM, text: "S"},
-                    {value: FONT_SIZE_MD, text: "M"},
-                    {value: FONT_SIZE_LG, text: "L"},
-                    {value: FONT_SIZE_XL, text: "XL"},
+                    {value: SIZE_KEYS.SMALL, text: "S"},
+                    {value: SIZE_KEYS.MEDIUM, text: "M"},
+                    {value: SIZE_KEYS.LARGE, text: "L"},
+                    {value: SIZE_KEYS.XLARGE, text: "XL"},
                 ],
             },
         },
-    // textAlign: {
-    //     type: "select",
-    //     props: {
-    //         title: "Text align",
-    //         values: {
-    //             // [TEXT_ALIGNS.LEFT]: ICONS.TEXT_LEFT,
-    //             // [TEXT_ALIGNS.CENTER]: ICONS.TEXT_CENTER,
-    //             // [TEXT_ALIGNS.RIGHT]: ICONS.TEXT_RIGHT,
-    //         },
-    //     },
-    // },
     },
-    // lineStart: {
-    //     type: "select",
-    //     props: {
-    //         title: "Line start",
-    //         values: {
-    //             // [LINE_CAPS.NONE]: ICONS.LINE_HORIZONTAL,
-    //             // [LINE_CAPS.ARROW]: ICONS.LINE_START_ARROW,
-    //             // [LINE_CAPS.CIRCLE]: ICONS.LINE_START_CIRCLE,
-    //             // [LINE_CAPS.SQUARE]: ICONS.LINE_START_SQUARE,
-    //         },
-    //     },
-    // },
-    // lineEnd: {
-    //     type: "select",
-    //     props: {
-    //         title: "Line end",
-    //         values: {
-    //             // [LINE_CAPS.NONE]: ICONS.LINE_HORIZONTAL,
-    //             // [LINE_CAPS.ARROW]: ICONS.LINE_END_ARROW,
-    //             // [LINE_CAPS.CIRCLE]: ICONS.LINE_END_CIRCLE,
-    //             // [LINE_CAPS.SQUARE]: ICONS.LINE_END_SQUARE
-    //         },
-    //     },
-    // },
-    // radius: {
-    //     type: "range",
-    //     props: {
-    //         title: "Radius",
-    //         domain: [0, 50],
-    //         step: 2,
-    //     },
-    // },
 };
 
 const getActiveTab = (values, currentTab) => {
@@ -199,13 +142,20 @@ const getActiveTab = (values, currentTab) => {
     });
 };
 
+const useValues = props => {
+    if (props.elements.length === 1) {
+        return props.elements[0];
+    }
+    // TODO: we need to compute common values for all elements
+    return props.values || {};
+};
+
 export const StyleDialog = props => {
-    const values = props.values || {};
+    const values = useValues(props);
     const [currentTab, setCurrentTab] = React.useState("fill");
-    const activeTab = React.useMemo(
-        () => getActiveTab(values, currentTab),
-        [values, currentTab],
-    );
+    const hasTextOption = checkHasTextOption(props);
+    const activeTab = getActiveTab(values, currentTab);
+
     return (
         <Dialog className="pt-4 right-0 top-0 pr-24" style={{paddingRight:"5rem"}}>
             <div className="d-flex gap-1 mb-4 b-solid b-1 b-gray-900 p-1 r-lg">
@@ -240,6 +190,7 @@ export const StyleDialog = props => {
 };
 
 StyleDialog.defaultProps = {
+    elements: [],
     values: {},
     onChange: null,
 };
