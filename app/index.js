7ffc55b09f15d00dfad9cacfb7f12e0073ba8b3a
Josemi <josemijuanes@gmail.com>
2022-12-18 12:24:24 +0100

    feat: using new folio board component in app entry

--- app/index.js
+++ app/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-
-import {Board} from "./components/Board.js";
+import Folio from "folio-board";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <div className="bg-white text-base c-dark position-fixed top-0 left-0 h-full w-full">
-        <Board />
+    <div className="bg-white text-base text-dark-700 position-fixed top-0 left-0 h-full w-full">
+        <Folio.Board />
     </div>
 );
