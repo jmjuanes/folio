9b5cd5c6b525ba4574f262caabc21d5433e5f32f
Josemi <josemijuanes@gmail.com>
2022-12-18 12:24:01 +0100

    fix: clean old app stuff

--- app/components/Screenshot.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Screenshot = props => {
-    return null;
-};
--- app/utils/image.js
+++
@@ -1,9 +0,0 @@
-// Create an image element
-// https://stackoverflow.com/a/4776378
-export const createImage = content => {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener("load", () => resolve(image));
-        image.src = content; // Set image source
-    });
-};
