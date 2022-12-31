b4475771f5802f78318bf84395b7178155cbdcf9
Josemi Juanes <jmjuanes@users.noreply.github.com>
2022-12-31 13:04:21 +0000

    feat: added export options

--- folio-board/components/SidePanels/ExportPanel.jsx
+++ folio-board/components/SidePanels/ExportPanel.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import {SidePanel} from "./SidePanel.jsx";
+import {Form} from "../Form/index.jsx";
+import {ImageIcon} from "../icons/index.jsx";
+
+const options = {
+    filename: {
+        type: "input",
+        title: "File name",
+        placeholder: "export.png",
+    },
+    background: {
+        type: "checkbox",
+        title: "Backgorund",
+    },
+    format: {
+        type: "select",
+        title: "Export format",
+        values: [
+            {value: "png", text: "PNG", icon: ImageIcon()},
+            {value: "svg", text: "SVG", icon: ImageIcon()},
+        ],
+        grid: "2",
+    },
+};
 
 export const ExportPanel = props => (
     <SidePanel title="Export" submitText="Export Image" onClose={props.onClose} onSubmit={props.onSubmit}>
-        Content
+        <Form
+            data={props.values || {}}
+            items={options}
+            onChange={props.onChange}
+        />
     </SidePanel>
 );
 
