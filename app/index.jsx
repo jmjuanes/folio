e33c66520d443b154e240fb564cc8802554939d3
Josemi <josemijuanes@gmail.com>
2023-01-11 21:18:11 +0100

    fix: using new exports in app

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import FolioBoard from "folio-board";
+import {Board} from "folio-board";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
     <div className="bg:white text:base text:dark-700 position:fixed top:0 left:0 h:full w:full">
-        <FolioBoard />
+        <Board />
     </div>
 );
