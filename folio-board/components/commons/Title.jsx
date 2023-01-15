1fe171bb437360d4fad52b028b87d6517c007eb6
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-01-15 00:30:13 +0000

    feat: allow to edit title in Title component

--- folio-board/components/commons/Title.jsx
+++ folio-board/components/commons/Title.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import {DEFAULT_TITLE} from "../../constants.js";
 
 export const Title = props => {
     const [editing, setEditing] = React.useState(false);
+    const inputRef = React.useRef(null);
+
+    React.useEffect(() => {
+        if (editing && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [editing]);
 
     return (
         <React.Fragment>
             {!editing && (
-                <div className="cursor:pointer text:lg" onClick={() => setEditing(false)}>
-                    <strong>{props.value || "Untitled"}</strong>
+                <div className="cursor:pointer text:lg" onClick={() => setEditing(true)}>
+                    <strong>{props.value || DEFAULT_TITLE}</strong>
                 </div>
             )}
             {editing && (
                 <input
+                    ref={inputRef}
                     type="text"
-                    className="bg:transparent b:none text:lg outline:0 p:0"
+                    className="bg:transparent b:none text:lg font:bold text:center outline:0 p:0"
                     defaultValue={props.value}
+                    onChange={event => props?.onChange?.(event.target.value || "")}
+                    onBlur={() => setEditing(false)}
                 />
             )}
         </React.Fragment>
@@ -22,6 +33,6 @@ export const Title = props => {
 };
 
 Title.defaultProps = {
-    value: "Untitled",
+    value: DEFAULT_TITLE,
     onChange: null,
 };
