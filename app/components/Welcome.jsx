a1abc56aca5cc88ac2dab1914b5c57b51888c355
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-05 15:59:40 +0000

    wip: initialize base welcome modal

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Rouct from "rouct";
+import {Modal} from "./commons/Modal.jsx";
+import {ProjectsList} from "./commons/ProjectsList.jsx";
 
-export const Welcome = props => {
-    return null;
-};
+export const Welcome = props => (
+    <Modal maxWidth="580px">
+        <div className="">
+            <div className="text:sm text:dark-100 o:80">Welcome to</div>
+            <div className="font:bold text:7xl">Folio.</div>
+            <div className="text:dark-100">A minimalistic digital whiteboard for sketching and prototyping.</div>
+        </div>
+        <div className="mt:16">
+            <ProjectsList />
+        </div>
+    </Modal>
+);
