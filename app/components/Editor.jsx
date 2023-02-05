636e4996540fbcd22432b858bc96f2b4a4ab40ec
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-05 23:04:04 +0000

    feat: improved editor and loading overlay

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import classNames from "classnames";
 import {Board} from "folio-board";
 
 import {Header} from "./Header/index.jsx";
+import {Loading} from "./commons/Loading.jsx";
 import {useClient} from "../contexts/ClientContext.jsx";
 import {useDebounce} from "../hooks/useDebounce.js";
 import {useDelay} from "../hooks/useDelay.js";
 
 export const Editor = props => {
     const [state, setState] = React.useState(null);
+    const [loadingVisible, setLoadingVisible] = React.useState(true);
     const client = useClient();
+    const classList = classNames({
+        "position:fixed top:0 left:0 h:full w:full": true,
+        "bg:white text:base text:dark-700": true,
+        "blur:md": !props.id || loadingVisible,
+    });
 
     // Debounce the data saving to database
     useDebounce(state, 250, () => {
@@ -19,31 +27,40 @@ export const Editor = props => {
     useDelay(1000, () => {
         client.get(props.id).then(project => {
             setState({...project});
+            setLoadingVisible(false);
         });
     });
 
     return (
-        <div className="bg:white text:base text:dark-700 position:fixed top:0 left:0 h:full w:full">
-            <Board
-                key={state?.id || "draft"}
-                elements={state?.elements || []}
-                assets={state?.assets || {}}
-                background={state?.background}
-                grid={state?.grid ?? true}
-                header={(
-                    <Header
-                        title={state?.title || "Untitled"}
-                        exportDisabled={(state?.elements || []).length === 0}
-                        onExport={format => {
-                            return null;
-                        }}
-                    />
-                )}
-                onChange={newData => {
-                    setState(prev => ({...prev, ...newData}));
-                }}
-            />
-        </div>
+        <React.Fragment>
+            <div className={classList}>
+                <Board
+                    key={state?.id || "draft"}
+                    elements={state?.elements || []}
+                    assets={state?.assets || {}}
+                    background={state?.background}
+                    grid={state?.grid ?? true}
+                    header={(
+                        <Header
+                            title={state?.title || "Untitled"}
+                            onTitleChange={newTitle => {
+                                setState(prev => ({...prev, title: newTitle}));
+                            }}
+                            exportDisabled={(state?.elements || []).length === 0}
+                            onExport={format => {
+                                return null;
+                            }}
+                        />
+                    )}
+                    onChange={newData => {
+                        setState(prev => ({...prev, ...newData}));
+                    }}
+                />
+            </div>
+            {loadingVisible && props.id && (
+                <Loading />
+            )}
+        </React.Fragment>
     );
 };
 
--- app/components/commons/Loading.jsx
+++ app/components/commons/Loading.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import classNames from "classnames";
 import {DrawingIcon} from "@mochicons/react";
-// import {Logo} from "./Logo.jsx";
 
-export const Loading = () => (
-    <div className="position:fixed w:full h:full top:0 left:0 bg:white d:flex items:center justify:center">
-        <div className="text:4xl text:dark-700 animation:pulse">
-            <DrawingIcon />
+export const Loading = () => {
+    const classList = classNames({
+        "d:flex flex:col items:center justify:center": true,
+        "position:absolute top:0 left:0 w:full h:full": true,
+        // "bg:white text:dark-700": !props.overlay,
+        "text:white": true, // props.overlay,
+    });
+
+    return (
+        <div className="position:fixed w:full h:full top:0 left:0 z:10">
+            <div className="position:absolute w:full h:full bg:dark-700 o:50 top:0 left:0" />
+            <div className={classList}>
+                <div className="text:4xl animation:pulse">
+                    <DrawingIcon />
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+};
