144d7fa5590408f2338190f8dda90b5d2457404e
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-05 15:59:27 +0000

    feat: improve loading component

--- app/components/commons/Loading.jsx
+++ app/components/commons/Loading.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {Logo} from "./Logo.jsx";
+import {DrawingIcon} from "@mochicons/react";
+// import {Logo} from "./Logo.jsx";
 
-export const Loading = props => (
+export const Loading = () => (
     <div className="position:fixed w:full h:full top:0 left:0 bg:white d:flex items:center justify:center">
-        <div className="">
-            <Logo size="md" animated={true} />
+        <div className="text:4xl text:dark-700 animation:pulse">
+            <DrawingIcon />
         </div>
     </div>
 );
