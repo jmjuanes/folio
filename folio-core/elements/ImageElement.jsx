56e3bc1d60670f92b5964ba5ab8b7acf9360cf48
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-11 13:16:30 +0000

    Fix image element for getting the correct asset data

--- folio-core/elements/ImageElement.jsx
+++ folio-core/elements/ImageElement.jsx
@@ -3,10 +3,8 @@ import {useAssets} from "../contexts/AssetsContext.jsx";
 
 export const ImageElement = props => {
     const assets = useAssets();
-    // const x = Math.min(props.x1, props.x2);
-    // const y = Math.min(props.y1, props.y2);
-    // const width = Math.abs(props.x2 - props.x1);
-    // const height = Math.abs(props.y2 - props.y1);
+    const dataUrl = assets[props.assetId]?.dataUrl || "";
+
     return (
         <g transform="">
             <image
@@ -15,7 +13,7 @@ export const ImageElement = props => {
                 y={Math.min(props.y1, props.y2)}
                 width={Math.abs(props.x2 - props.x1)}
                 height={Math.abs(props.y2 - props.y1)}
-                href={assets[props.assetId] || ""}
+                href={dataUrl}
                 onPointerDown={props.onPointerDown}
                 onDoubleClick={props.onDoubleClick}
             />
