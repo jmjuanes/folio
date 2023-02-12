229839bc439f4799bcfa92abeb2369371e5fe466
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-12 21:38:44 +0100

    Some improvements in menu component

--- folio-board/components/View/Menu.jsx
+++ folio-board/components/View/Menu.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import classNames = require("classnames");
+import {Button} from "../commons/Button.jsx";
+import {Dropdown} from "../commons/Dropdown.jsx";
 
-export const Menu = props => {
-    const dropdownClass = classNames({
-        "d:none d:block:group-focus-within": true,
-        "top:full left:0": props.align === "left",
-        "top:full right:0": props.align === "right",
-    });
-    return (
-        <div className="d:flex position:relative group" tabIndex="0">
-            <Button
-                text={props.text}
-                icon={props.icon}
-            />
+export const Menu = props => (
+    <div className="d:flex position:relative group" tabIndex="0">
+        <Button
+            text={props.text}
+            icon={props.icon}
+            disabled={props.disabled}
+        />
+        {!props.disabled && (
             <Dropdown
-                className={dropdownClass}
+                className={classNames({
+                    "d:none d:block:group-focus-within": true,
+                    "top:full left:0": props.align === "left",
+                    "top:full right:0": props.align === "right",
+                })}
                 items={props.items}
             />
-        </div>
-    );
-};
+        )}
+    </div>
+);
 
 Menu.defaultProps = {
     align: "left",
     text: null,
     icon: null,
+    disabled: false,
     items: [],
 };
