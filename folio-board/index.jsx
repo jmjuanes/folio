b049af191bf4f95f711d3bbc0e2bcf667908663a
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-13 00:18:42 +0100

    Fix exports in main folio-board entry

--- folio-board/index.jsx
+++ folio-board/index.jsx
@@ -3,8 +3,14 @@ import "lowcss/dist/low.css";
 // Export presets
 export * from "./presets/Board.jsx";
 
+// Export contexts
+export * from "./contexts/BoardContext.jsx";
+export * from "./contexts/ConfirmContext.jsx";
+export * from "./contexts/ToastContext.jsx"
+
 // Export components
 export * from "./components/commons/index.jsx";
 export * from "./components/Form/index.jsx";
 export * from "./components/Dialogs/index.jsx";
 export * from "./components/Panels/index.jsx";
+export * from "./components/View/index.jsx";
