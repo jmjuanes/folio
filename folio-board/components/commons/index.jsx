54ba65cb4c28bc12f8a2daf6dc39fd5ebcbe6ae0
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-12 18:57:52 +0000

    Moved modal component to commons folder of folio-board

--- folio-board/components/commons/index.jsx
+++ folio-board/components/commons/index.jsx
@@ -1,5 +1,6 @@
 export * from "./Button.jsx";
 export * from "./Dropdown.jsx";
 export * from "./Layout.jsx";
+export * from "./Modal.jsx";
 export * from "./Renderer.jsx";
 export * from "./FileInput.jsx";
