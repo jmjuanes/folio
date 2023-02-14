f27fcb9fda81b3c3898549a5a4bd6cbcc0663eca
Josemi Juanes <jmjuanes@users.noreply.github.com>
2023-02-15 00:13:04 +0100

    Fixed panel tests

--- folio-board/components/Panels/Panel.test.jsx
+++ folio-board/components/Panels/Panel.test.jsx
@@ -1,24 +1,29 @@
 import {create} from "react-test-renderer";
-
 import {Panel, PanelButton} from "./Panel.jsx";
 
 describe("Panel", () => {
     it("should render", () => {
-        const component = create(<Panel position="top-left" />);
+        const component = create((
+            <Panel position="top-left" />
+        ));
 
-        expect(component.toJSON).toMatchSnapshot();
+        expect(component.toJSON()).toMatchSnapshot();
     });
 });
 
 describe("PanelButton", () => {
     it("should render", () => {
-        const component = create(<Panel position="top-left" />);
+        const component = create((
+            <PanelButton>Hello</PanelButton>
+        ));
 
-        expect(component.toJSON).toMatchSnapshot();
+        expect(component.toJSON()).toMatchSnapshot();
     });
 
     it("should have the 'is-active' class when the active prop is 'true'", () => {
-        const component = create(<PanelButton active={true} />);
+        const component = create((
+            <PanelButton active={true} />
+        ));
         const root = component.root;
 
         expect(root.findByType("div").props.className).toEqual(
@@ -27,7 +32,9 @@ describe("PanelButton", () => {
     });
 
     it("should have the 'is-disabled' class when the disabled prop is 'true'", () => {
-        const component = create(<PanelButton disabled={true} />);
+        const component = create((
+            <PanelButton disabled={true} />
+        ));
         const root = component.root;
 
         expect(root.findByType("div").props.className).toEqual(
