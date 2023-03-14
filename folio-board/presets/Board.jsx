12729c7719be57870130ef1240045e02f61f8de6
Josemi <josemijuanes@gmail.com>
2023-03-14 21:19:46 +0100

    feat: improved default board preset interface and props

--- folio-board/presets/Board.jsx
+++ folio-board/presets/Board.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {BarsIcon, DownloadIcon, FolderIcon, TrashIcon} from "@mochicons/react";
 import {ImageIcon, CodeIcon, CameraIcon} from "@mochicons/react";
-import {LockIcon, GridIcon} from "@mochicons/react";
+import {ToolIcon, LockIcon, GridIcon} from "@mochicons/react";
 import {EXPORT_FORMATS, BACKGROUND_COLORS} from "folio-core";
 import {ACTIONS} from "../constants.js";
 import {BoardProvider, useBoard} from "../contexts/BoardContext.jsx";
@@ -9,9 +9,12 @@ import {Layout, Renderer, SecondaryButton} from "../components/commons/index.jsx
 import {Dropdown, DropdownSeparator, DropdownGroup} from "../components/commons/index.jsx";
 import {DropdownItem, DropdownCheckItem, DropdownLinkItem} from "../components/commons/index.jsx";
 import {ColorPicker} from "../components/Form/ColorPicker.jsx";
+import {Welcome} from "../components/Layout/Welcome.jsx";
 
 const BoardWrapper = props => {
     const board = useBoard();
+    const [welcomeVisible, setWelcomeVisible] = React.useState(props.showWelcome && (board.elements.length === 0));
+
     return (
         <div className="position:relative overflow:hidden h:full w:full">
             <Renderer
@@ -19,43 +22,42 @@ const BoardWrapper = props => {
                 onScreenshot={props.onScreenshot}
             />
             <Layout
-                grid={props.grid}
-                background={props.background}
                 header={true}
                 headerLeftContent={(
                     <div className="d:flex gap:2">
                         <div className="d:flex position:relative group" tabIndex="0">
                             <SecondaryButton icon={(<BarsIcon />)} />
                             <Dropdown className="d:none d:block:group-focus-within top:full left:0">
-                                {props.boardActions?.load !== false && (
+                                <DropdownGroup title="Actions" />
+                                {props.showLoadAction && (
                                     <DropdownItem
                                         icon={(<FolderIcon />)}
                                         text="Open..."
                                         onClick={props.onLoad}
                                     />
                                 )}
-                                {props.boardActions?.save !== false && (
+                                {props.showOpenAction && (
                                     <DropdownItem
                                         icon={(<DownloadIcon />)}
                                         text="Save as..."
                                         onClick={props.onSave}
                                     />
                                 )}
-                                {props.boardActions?.reset !== false && (
+                                {props.showResetBoardAction && (
                                     <DropdownItem
                                         icon={(<TrashIcon />)}
                                         text="Reset the board"
                                         onClick={props.onReset}
                                     />
                                 )}
-                                {props.boardActions?.settings !== false && (
+                                {props.showBoardSettings && (
                                     <React.Fragment>
                                         <DropdownSeparator />
-                                        <DropdownGroup title="General settings" />
+                                        <DropdownGroup title="Board Settings" />
                                         <DropdownCheckItem
                                             active={board.grid}
                                             icon={(<GridIcon />)}
-                                            text="Grid"
+                                            text="Show Grid"
                                             onClick={() => {
                                                 board.grid = !board.grid;
                                                 board.update();
@@ -64,43 +66,30 @@ const BoardWrapper = props => {
                                                 });
                                             }}
                                         />
-                                        <DropdownCheckItem
-                                            active={board.lockTool}
-                                            icon={(<LockIcon />)}
-                                            text="Lock tool"
-                                            onClick={() => {
-                                                board.lockTool = !board.lockTool;
+                                    </React.Fragment>
+                                )}
+                                {props.showChangeBackground && (
+                                    <React.Fragment>
+                                        <DropdownSeparator />
+                                        <DropdownGroup title="Background" />
+                                        <ColorPicker
+                                            value={board.background}
+                                            values={Object.values(BACKGROUND_COLORS)}
+                                            onChange={newBackground => {
+                                                board.background = newBackground;
                                                 board.update();
                                                 props.onChange?.({
-                                                    lockTool: board.lockTool,
+                                                    background: board.background,
                                                 });
                                             }}
                                         />
                                     </React.Fragment>
                                 )}
-                                {props.boardActions?.background !== false && (
+                                {(props.showLinks && props.links?.length > 0) && (
                                     <React.Fragment>
                                         <DropdownSeparator />
-                                        <div className="d:block mt:2">
-                                            <DropdownGroup title="Background" />
-                                            <ColorPicker
-                                                value={board.background}
-                                                values={Object.values(BACKGROUND_COLORS)}
-                                                onChange={newBackground => {
-                                                    board.background = newBackground;
-                                                    board.update();
-                                                    props.onChange?.({
-                                                        background: board.background,
-                                                    });
-                                                }}
-                                            />
-                                        </div>
-                                    </React.Fragment>
-                                )}
-                                {props.customLinks?.length > 0 && (
-                                    <React.Fragment>
-                                        <DropdownSeparator />
-                                        {props.customLinks.map(link => (
+                                        <DropdownGroup title="Links" />
+                                        {props.links.map(link => (
                                             <DropdownLinkItem
                                                 key={link.url}
                                                 url={link.url}
@@ -117,7 +106,7 @@ const BoardWrapper = props => {
                 headerRightContent={(
                     <div className="d:flex gap:2">
                         {props.customHeaderRightContent}
-                        {props.boardActions?.screenshot !== false && (
+                        {props.showScreenshot && (
                             <SecondaryButton
                                 icon={(<CameraIcon />)}
                                 disabled={board.elements.length === 0}
@@ -130,7 +119,7 @@ const BoardWrapper = props => {
                                 }}
                             />
                         )}
-                        {(props.boardActions?.exportPng !== false || props.boardActions?.exportSvg !== false) && (
+                        {props.showExport && (
                             <div className="d:flex position:relative group" tabIndex="0">
                                 <SecondaryButton
                                     icon={(<ImageIcon />)}
@@ -138,22 +127,18 @@ const BoardWrapper = props => {
                                     disabled={board.elements.length === 0}
                                 />
                                 <Dropdown className="d:none d:block:group-focus-within top:full right:0">
-                                    {props.boardActions?.exportPng !== false && (
-                                        <DropdownItem
-                                            icon={(<ImageIcon />)}
-                                            text="Export as PNG"
-                                            disabled={board.elements.length === 0}
-                                            onClick={() => props.onExport?.(EXPORT_FORMATS.PNG)}
-                                        />
-                                    )}
-                                    {props.boardActions?.exportSvg !== false && (
-                                        <DropdownItem
-                                            icon={(<CodeIcon />)}
-                                            text="Export as SVG"
-                                            disabled={board.elements.length === 0}
-                                            onClick={() => props.onExport?.(EXPORT_FORMATS.SVG)}
-                                        />
-                                    )}
+                                    <DropdownItem
+                                        icon={(<ImageIcon />)}
+                                        text="Export as PNG"
+                                        disabled={board.elements.length === 0}
+                                        onClick={() => props.onExport?.(EXPORT_FORMATS.PNG)}
+                                    />
+                                    <DropdownItem
+                                        icon={(<CodeIcon />)}
+                                        text="Export as SVG"
+                                        disabled={board.elements.length === 0}
+                                        onClick={() => props.onExport?.(EXPORT_FORMATS.SVG)}
+                                    />
                                 </Dropdown>
                             </div>
                         )}
@@ -162,13 +147,20 @@ const BoardWrapper = props => {
                 footer={false}
                 onChange={props.onChange}
             />
+            {welcomeVisible && (
+                <Welcome
+                    version={process.env.VERSION}
+                    onClose={() => setWelcomeVisible(false)}
+                    onLoad={props.onLoad}
+                />
+            )}
         </div>
     );
 };
 
 export const Board = props => (
     <BoardProvider
-        initialState={props.initialState}
+        initialData={props.initialData}
         render={() => ((
             <BoardWrapper {...props} />
         ))}
@@ -176,15 +168,23 @@ export const Board = props => (
 );
 
 Board.defaultProps = {
-    initialtate: {},
-    boardActions: {},
+    initialData: {},
+    links: [],
     customHeaderLeftContent: null,
     custonHeaderRightContent: null,
-    customLinks: [],
     onChange: null,
     onExport: null,
     onSave: null,
     onLoad: null,
     onReset: null,
     onScreenshot: null,
+    showWelcome: true,
+    showScreenshot: true,
+    showExport: true,
+    showLinks: true,
+    showLoadAction: true,
+    showOpenAction: true,
+    showResetBoardAction: true,
+    showBoardSettings: true,
+    showChangeBackground: true,
 };
