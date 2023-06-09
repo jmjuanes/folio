19cd6c9fddb0713002301af93e1ce58399a05e06
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-06-09 22:44:41 +0000

    Fix exported values in folio entrypoint

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 // TODO: we need to remove this import and add a new styles.css file
 import "lowcss/dist/low.css";
 
+// Export constans
+export {VERSION} from "./constants.js";
+
 // Export presets
 export * from "./presets/Board.jsx";
 
 // Export contexts
 export * from "./contexts/BoardContext.jsx";
 export * from "./contexts/ConfirmContext.jsx";
-export * from "./contexts/ToastContext.jsx"
 
 // Export utilities to handle folio data
 export * from "./export.js";
