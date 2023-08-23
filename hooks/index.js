866d5befc0d1e386a0d8a1deaa2c0492e2b1727e
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-08-23 09:59:25 +0000

    Added delay and debounce hooks

--- hooks/index.js
+++ hooks/index.js
@@ -1,4 +1,26 @@
+import React from "react";
+
 export * from "./useBounds.js";
 export * from "./useCursor.js";
 export * from "./useEvents.js";
 export * from "./useHandlers.js";
+
+// Delay the execution of the provided function when the component is mounted
+export const useDelay = (ms, callback) => {
+    React.useEffect(() => {
+        setTimeout(callback, ms);
+    }, []);
+};
+
+// Debounce the execution of the provided function
+export const useDebounce = (ms, callback) => {
+    const timer = React.useRef(null);
+    return (args) => {
+        // First check to clear the current timeout
+        if (timer.current !== null) {
+            clearTimeout(timer.current);
+        }
+        // Register the new timeout
+        timer.current = setTimeout(() => callback(args), ms);
+    };
+};
