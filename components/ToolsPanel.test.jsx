63603204eef6f7b610eb30f3c097fbb9c51400b3
Josemi <josemijuanes@gmail.com>
2023-08-27 17:40:02 +0200

    Fix ToolsPanel test

--- components/ToolsPanel.test.jsx
+++ components/ToolsPanel.test.jsx
@@ -1,6 +1,10 @@
 import {render, screen} from "@testing-library/react";
 import {ToolsPanel} from "./ToolsPanel.jsx";
 
+jest.mock("@josemi-icons/react", () => ({
+    NoteIcon: jest.fn(() => "NOTE_ICON"),
+}));
+
 jest.mock("../contexts/BoardContext.jsx", () => ({
     useBoard: jest.fn(() => ({
         defaults: {},
