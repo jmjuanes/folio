ada32cbd9fbdd5cbcb303f314ca08df9f2a1a825
Josemi <josemijuanes@gmail.com>
2023-08-28 19:20:19 +0200

    Allow to specify a padding to text elements

--- elements/TextElement.jsx
+++ elements/TextElement.jsx
@@ -9,10 +9,11 @@ const stopEventPropagation = event => {
 
 export const TextElement = props => {
     const inputRef = React.useRef(null);
+    const padding = props.padding ?? 0;
     const x = (props.x1 + props.x2) / 2;
     const y = (props.y1 + props.y2) / 2;
-    const width = Math.abs(props.x2 - props.x1);
-    const height = Math.abs(props.y2 - props.y1);
+    const width = Math.abs(props.x2 - props.x1) - (2 * padding);
+    const height = Math.abs(props.y2 - props.y1) - (2 * padding);
     const textSize = props.textSize ?? 0;
     const textFont = props.textFont ?? FONT_FACES.SANS;
     const textColor = props.textColor ?? BLACK;
@@ -37,12 +38,14 @@ export const TextElement = props => {
         userSelect: "none",
         wordBreak: "break-all",
         opacity: props.opacity,
+        overflow: "hidden",
     };
 
     // Positions for the foreignObject to contain the text or the input
     const foreignX = (-1) * width / 2;
-    const foreignY = (-1) * props.textHeight / 2;
-    const foreignWidth = Math.max(width, GRID_SIZE);
+    const foreignY = Math.max(-height / 2, (-1) * (props.verticalAlign === "top" ? height : props.textHeight) / 2);
+    const foreignWidth = Math.max(width, 1);
+    const foreignHeight = Math.max(0, Math.min(props.textHeight, height));
 
     return (
         <g transform={`translate(${x} ${y})`}>
@@ -61,20 +64,20 @@ export const TextElement = props => {
                 />
             )}
             {props.text && !props.editing && (
-                <foreignObject x={foreignX} y={foreignY} width={foreignWidth} height={props.textHeight}>
+                <foreignObject x={foreignX} y={foreignY} width={foreignWidth} height={foreignHeight}>
                     <div style={previewStyles}>
                         {props.text}
                     </div>
                 </foreignObject>
             )}
             {props.editing && (
-                <foreignObject x={foreignX} y={foreignY} width={foreignWidth} height={props.textHeight}>
+                <foreignObject x={foreignX} y={foreignY} width={foreignWidth} height={foreignHeight}>
                     <textarea
                         ref={inputRef}
                         wrap="off"
                         defaultValue={props.text || ""}
                         style={{
-                            width: Math.max(width, GRID_SIZE) + "px",
+                            width: Math.max(width, 1) + "px",
                             height: props.textHeight + "px",
                             backgroundColor: "transparent",
                             border: "0px solid transparent",
@@ -120,8 +123,8 @@ export const TextElement = props => {
                     data-element={props.id}
                     x={(-1) * width / 2}
                     y={(-1) * height / 2}
-                    width={width}
-                    height={height}
+                    width={Math.max(width, 0)}
+                    height={Math.max(height, 0)}
                     fill="transparent"
                     stroke="none"
                     onPointerDown={props.onPointerDown}
@@ -134,7 +137,9 @@ export const TextElement = props => {
 
 TextElement.defaultProps = {
     text: "",
+    padding: 0,
     embedded: false,
+    verticalAlign: "center",
     onChange: null,
     onPointerDown: null,
     onDoubleClick: null,
