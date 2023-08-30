ff98cecaf6404333b62b787ef98071c26e052062
Josemi <josemijuanes@gmail.com>
2023-08-30 19:48:06 +0200

    Minor imporvements in loading screen

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import {DrawingIcon} from "@josemi-icons/react";
+import {LoaderIcon} from "@josemi-icons/react";
 
 export const Loading = () => (
     <div className="fixed w-full h-full top-0 left-0 z-10">
-        <div className="flex items-center justify-center bg-white w-full h-full">
-            <div className="animation-pulse">
-                <div className="flex p-3 rounded-xl bg-gray-900 text-white text-6xl">
-                    <DrawingIcon />
+        <div className="flex flex-col items-center justify-center bg-white w-full h-full">
+            <div className="font-black text-6xl text-gray-900 font-crimson tracking-tight leading-none">
+                <span>Folio.</span>
+            </div>
+            <div className="flex mt-2">
+                <div className="flex animation-spin text-gray-500">
+                    <LoaderIcon />
                 </div>
             </div>
         </div>
