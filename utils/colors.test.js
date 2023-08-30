57c888fc6575e62560a4a6025251bbb01213254d
Josemi <josemijuanes@gmail.com>
2023-08-30 19:45:23 +0200

    Added test for colorShade function

--- utils/colors.test.js
+++ utils/colors.test.js
@@ -1,4 +1,4 @@
-import {isValidHexColor} from "./colors.js";
+import {isValidHexColor, colorShade} from "./colors.js";
 
 describe("isValidHExColor", () => {
     it("should return 'true' if the provided value is a hex color", () => {
@@ -15,5 +15,15 @@ describe("isValidHExColor", () => {
         nonValidHexColors.forEach(value => {
             expect(isValidHexColor(value)).toBeFalsy();
         });
+    });
+});
+
+describe("colorShade", () => {
+    it("should return a lighter color than provided", () => {
+        expect(colorShade("#ffffff", -20)).toEqual("#ebebeb");
+    });
+
+    it("should return a darker color than provided", () => {
+        expect(colorShade("#000000", 20)).toEqual("#141414");
     });
 });
