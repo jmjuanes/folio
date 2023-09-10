c986f0639b563d727117bd84bce11d8ebb1f4c6e
Josemi <josemijuanes@gmail.com>
2023-09-10 12:12:43 +0200

    Fix path to styles in static content

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const package = require("./package.json");
 
 module.exports = {
@@ -89,6 +90,11 @@ module.exports = {
             template: path.join(__dirname, "index.html"),
             filename: "app.html",
             inject: true,
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                path.join(__dirname, "node_modules", "lowcss", "dist/low.css"),
+            ],
         }),
     ],
 };
