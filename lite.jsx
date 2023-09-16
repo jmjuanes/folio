00a4fc746f957a08ac29b8abd0937325871102fe
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-09-16 14:37:51 +0000

    fix: migrate stored board when a new version is launched

--- lite.jsx
+++ lite.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import {VERSION} from "./constants.js";
 import {loadFromJson, saveAsJson} from "./board/json.js";
+import {migrate} from "./board/migrate.js";
 import {Board} from "./components/Board.jsx";
 import {useLocalStorage} from "./hooks/useLocalStorage.js";
 
@@ -8,12 +10,20 @@ import "lowcss/dist/low.css";
 
 // Folio Lite component
 const FolioLite = () => {
-    const [state, setState] = useLocalStorage("folio-lite-data", {createdAt: Date.now()});
-
+    const [state, setState] = useLocalStorage("folio-lite", {
+        version: VERSION,
+        createdAt: Date.now(),
+    });
+    // Hook to migrate data after initialization
+    React.useEffect(() => {
+        if (state.version && state.version !== VERSION) {
+            setState(migrate(state, state.version));
+        }
+    }, [state.version]);
     return (
         <div className="fixed top-0 left-0 h-full w-full bg-white text-base text-gray-700">
             <Board
-                key={state?.createdAt}
+                key={(state.version || "") + "." + (state?.createdAt || "")}
                 initialData={() => {
                     return Promise.resolve(state);
                 }}
@@ -22,6 +32,7 @@ const FolioLite = () => {
                     {url: process.env.URL_ISSUES, text: "Report a bug"},
                 ]}
                 showLoad={true}
+                showWelcomeHint={true}
                 onChange={newState => {
                     return setState(prevState => ({
                         ...prevState,
