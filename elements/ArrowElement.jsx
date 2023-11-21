c3d3241b158400c5cf1dc2cbd7007bec89275c10
Josemi <josemijuanes@gmail.com>
2023-11-21 19:46:00 +0100

    Move arrowhead to a new element and fix curve path

--- elements/ArrowElement.jsx
+++ elements/ArrowElement.jsx
@@ -1,63 +1,9 @@
 import React from "react";
 import {STROKES, ARROWHEADS, BLACK, NONE, TRANSPARENT} from "../constants.js";
 import {OPACITY_FULL, OPACITY_NONE} from "../constants.js";
+import {Arrowhead} from "./shared/ArrowHead.jsx";
 import {getBalancedDash, getPointsDistance} from "../utils/math.js";
 import {getCurvePath} from "../utils/paths.js";
-
-const Arrowhead = props => {
-    const size = props.strokeWidth * 2 + 4;
-    const angle = Math.atan2(props.y - props.y2, props.x - props.x2);
-    const commands = [];
-
-    if (props.type === ARROWHEADS.ARROW || props.type === ARROWHEADS.TRIANGLE) {
-        const angle2 = Math.PI / 6;
-        const hip = size * 4 / 3;
-        commands.push(`M${props.x - hip * Math.cos(angle - angle2)},${props.y + hip * Math.sin(angle2 - angle)}`);
-        commands.push(`L${props.x},${props.y}`);
-        commands.push(`L${props.x - hip * Math.cos(angle2 + angle)},${props.y - hip * Math.sin(angle2 + angle)}`);
-        if (props.type === ARROWHEADS.TRIANGLE) {
-            commands.push("Z");
-        }
-    }
-    else if (props.type === ARROWHEADS.SQUARE) {
-        const angle2 = Math.atan(0.5); // Second angle for the rectangle
-        const hsize = size / 2; // Half of the size
-        const hip = Math.sqrt(size * size + hsize * hsize);
-        commands.push(`M${props.x},${props.y}`);
-        commands.push(`L${props.x - hsize * Math.sin(angle)},${props.y + hsize * Math.cos(angle)}`);
-        commands.push(`L${props.x - hip * Math.cos(angle - angle2)},${props.y + hip * Math.sin(angle2 - angle)}`);
-        commands.push(`L${props.x - hip * Math.cos(angle + angle2)},${props.y - hip * Math.sin(angle2 + angle)}`);
-        commands.push(`L${props.x + hsize * Math.sin(angle)},${props.y - hsize * Math.cos(angle)}`);
-        commands.push("Z");
-    }
-    else if (props.type === ARROWHEADS.SEGMENT) {
-        const hsize = size / 2; // Half of the size
-        commands.push(`M${props.x - hsize * Math.sin(angle)},${props.y + hsize * Math.cos(angle)}`);
-        commands.push(`L${props.x + hsize * Math.sin(angle)},${props.y - hsize * Math.cos(angle)}`);
-    }
-    else if (props.type === ARROWHEADS.CIRCLE) {
-        const hsize = size / 2; // Half of the size
-        const x2 = props.x - size * Math.cos(angle);
-        const y2 = props.y - size * Math.sin(angle);
-        commands.push(`M${props.x},${props.y}`);
-        commands.push(`A${hsize},${hsize} 0 1 1 ${x2},${y2}`);
-        commands.push(`A${hsize},${hsize} 0 1 1 ${props.x},${props.y}`);
-    }
-
-    return (
-        <path
-            data-element={props.id}
-            d={commands.join("")}
-            fill={TRANSPARENT}
-            stroke={props.strokeColor}
-            strokeWidth={props.strokeWidth}
-            strokeOpacity={props.strokeOpacity}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            onPointerDown={props.onPointerDown}
-        />
-    );
-};
 
 export const ArrowElement = props => {
     const x = Math.min(props.x1, props.x2);
@@ -82,10 +28,11 @@ export const ArrowElement = props => {
                 [props.x1 - x, props.y1 - y],
                 [props.x2 - x, props.y2 - y],
             ];
+            let controlPoint = null;
             if (typeof props.xCenter === "number") {
-                points.splice(1, 0, [props.xCenter - x, props.yCenter - y]);
+                controlPoint = [props.xCenter - x, props.yCenter - y];
             }
-            return getCurvePath(points);
+            return getCurvePath(points, controlPoint);
         },
         [props.x1, props.y1, props.xCenter, props.yCenter, props.x2, props.y2],
     );
@@ -136,8 +83,8 @@ export const ArrowElement = props => {
                     type={props.startArrowhead}
                     x={props.x1 - x}
                     y={props.y1 - y}
-                    x2={props.x2 - x}
-                    y2={props.y2 - y}
+                    x2={(props.xCenter ?? props.x2) - x}
+                    y2={(props.yCenter ?? props.y2) - y}
                     strokeWidth={strokeWidth}
                     strokeColor={strokeColor}
                     strokeOpacity={strokeOpacity}
@@ -150,8 +97,8 @@ export const ArrowElement = props => {
                     type={props.endArrowhead}
                     x={props.x2 - x}
                     y={props.y2 - y}
-                    x2={props.x1 - x}
-                    y2={props.y1 - y}
+                    x2={(props.xCenter ?? props.x1) - x}
+                    y2={(props.yCenter ?? props.y1) - y}
                     strokeWidth={strokeWidth}
                     strokeColor={strokeColor}
                     strokeOpacity={strokeOpacity}
@@ -160,9 +107,10 @@ export const ArrowElement = props => {
             )}
             <path 
                 data-element={props.id}
-                d={selectionPath}
+                d={arrowPath}
                 fill={TRANSPARENT}
-                stroke={NONE}
+                stroke={TRANSPARENT}
+                strokeWidth={Math.max(strokeWidth, 2) * 4}
                 onPointerDown={props.onPointerDown}
             />
         </g>
