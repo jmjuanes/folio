86fe02449fcbfd25641bfbd3aa7202fd4c1bb766
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-26 20:53:30 +0000

    Fix modal maximum width

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import classNames from "classnames";
 import {CloseIcon} from "@josemi-icons/react";
 
 export const Modal = props => (
     <React.Fragment>
         <div className="fixed w-full h-full bg-white o-70 z-9 top-0 left-0" />
         <div className="fixed w-full h-full flex items-center justify-center z-10 top-0 left-0">
-            <div className="rounded-lg border border-neutral-200 bg-white w-full p-6 shadow-sm" style={{maxWidth: props.maxWidth}}>
+            <div className={classNames(props.className, "rounded-lg border border-neutral-200 bg-white w-full p-6 shadow-sm")}>
                 <div className="flex items-center justify-between mb-3 select-none">
                     <div className="flex items-center text-lg font-bold text-neutral-900">
                         <span>{props.title}</span>
@@ -23,7 +24,7 @@ export const Modal = props => (
 );
 
 Modal.defaultProps = {
+    className: "max-w-lg",
     title: "",
-    maxWidth: "500px",
     onClose: null,
 };
