a201363225dacd953f8a3954a836851dd351bd29
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-26 23:22:06 +0000

    Improve boards list

--- components/BoardList.jsx
+++ components/BoardList.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import {DownloadIcon, DrawingIcon, ImageSlashIcon, renderIcon} from "@josemi-icons/react";
-import {EditIcon, DotsVerticalIcon, TrashIcon, CopyIcon} from "@josemi-icons/react";
+import {DotsVerticalIcon, TrashIcon, CopyIcon} from "@josemi-icons/react";
+import {PrimaryButton} from "./Button.jsx";
 import {Dropdown, DropdownItem, DropdownSeparator} from "./Dropdown.jsx";
 import {useDelay} from "../hooks/index.js";
 
 const BoardEmpty = props => (
-    <div className="select-none border-2 border-dashed border-gray-200 w-full rounded-xl">
+    <div className="select-none bg-neutral-100 w-full rounded-lg">
         <div className="w-full py-16 flex flex-col items-center">
-            <div className="flex text-7xl text-gray-900">
+            <div className="flex text-7xl text-neutral-950">
                 <DrawingIcon />
             </div>
-            <div className="font-black text-4xl text-gray-900 mt-2 font-crimson tracking-tight">
-                <span>Let's start sketching.</span>
+            <div className="font-bold text-2xl text-neutral-900 mt-2">
+                <span>No boards yet...</span>
             </div>
-            <div className="text-sm text-gray-600">
+            <div className="text-sm text-neutral-600">
                 Create your first board to start sketching.
             </div>
             <div className="mt-6">
-                <div className="flex rounded-full border-2 border-gray-900 hover:bg-gray-900 hover:text-white cursor-pointer px-4 py-2" onClick={props.onCreate}>
-                    <strong className="text-sm">Create a new board</strong>
-                </div>
+                <PrimaryButton
+                    onClick={props.onCreate}
+                    text="Create a new board"
+                    textClassName="text-sm font-bold"
+                />
             </div>
             <div className="mt-1">
                 <div className="text-center hover:underline cursor-pointer" onClick={props.onLoad}>
-                    <span className="text-xs text-gray-600">Or import from a local file...</span>
+                    <span className="text-xs text-neutral-600">Or import from a local file...</span>
                 </div>
             </div>
         </div>
@@ -34,7 +37,7 @@ const BoardItem = props => (
     <div className="w-full select-none relative">
         <div className="absolute top-0 right-0 mt-1 mr-1">
             <div className="flex relative group" tabIndex="0">
-                <div className="flex bg-white hover:bg-gray-200 group-focus-within:bg-gray-900 rounded-full p-2 cursor-pointer">
+                <div className="flex bg-white hover:bg-neutral-200 group-focus-within:bg-neutral-900 rounded-full p-2 cursor-pointer">
                     <div className="flex group-focus-within:text-white">
                         <DotsVerticalIcon />
                     </div>
@@ -60,33 +63,26 @@ const BoardItem = props => (
             </div>
         </div>
         <div className="w-full cursor-pointer" onClick={props.onClick}>
-            <div className="w-full h-32 rounded-lg border-2 border-gray-900 flex items-center justify-center">
-                <div className="text-gray-700 text-5xl flex">
+            <div className="w-full h-32 rounded-md bg-neutral-100 flex items-center justify-center">
+                <div className="text-neutral-700 text-5xl flex">
                     <ImageSlashIcon />
                 </div>
             </div>
         </div>
         <div className="flex items-center justify-between flex-nowrap mt-2">
-            <div className="cursor-pointer text-center w-full" onClick={props.onClick}>
+            <div className="cursor-pointer w-full" onClick={props.onClick}>
                 <span className="font-bold">{props.title || "untitled"}</span>
             </div>
         </div>
-        <div className="flex flex-nowrap gap-3 justify-center">
-            <div className="flex items-center gap-1 text-gray-500">
-                <div className="text-xs flex items-center">
-                    <EditIcon />
-                </div>
-                <div className="text-xs flex items-center">
-                    <span>Updated on <b>{(new Date(props.updatedAt)).toLocaleDateString()}</b></span>
-                </div>
-            </div>
+        <div className="text-neutral-500 text-2xs flex items-center">
+            <span>Updated on <b>{(new Date(props.updatedAt)).toLocaleDateString()}</b></span>
         </div>
     </div>
 );
 
 const BoardActionButton = props => (
     <div className="cursor-pointer" onClick={props.onClick}>
-        <div className="rounded-md hover:bg-gray-200 text-gray-600 flex items-center gap-1 p-2">
+        <div className="rounded-md hover:bg-neutral-200 text-neutral-600 hover:text-neutral-800 flex items-center gap-1 p-2">
             <div className="text-lg flex">
                 {renderIcon(props.icon)}
             </div>
@@ -136,8 +132,8 @@ export const BoardList = props => {
                 <div className="grid grid-cols-4 gap-8 select-none">
                     {[1,2,3,4].map(key => (
                         <div key={key} className="w-full animation-pulse">
-                            <div className="w-full h-32 bg-gray-300 rounded-lg" />
-                            <div className="w-full h-8 bg-gray-300 rounded-lg mt-4" />
+                            <div className="w-full h-32 bg-neutral-200 rounded-lg" />
+                            <div className="w-full h-8 bg-neutral-200 rounded-lg mt-4" />
                         </div>
                     ))}
                 </div>
