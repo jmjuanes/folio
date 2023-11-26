8ea6b4a0a950cfe85602a91b9c63e81ded2c6b8e
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-26 20:35:22 +0000

    Improve modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -3,18 +3,19 @@ import {CloseIcon} from "@josemi-icons/react";
 
 export const Modal = props => (
     <React.Fragment>
-        <div className="fixed w-full h-full bg-gray-900 o-50 z-9 top-0 left-0" />
+        <div className="fixed w-full h-full bg-white o-70 z-9 top-0 left-0" />
         <div className="fixed w-full h-full flex items-center justify-center z-10 top-0 left-0">
-            <div className="rounded-xl border-2 border-gray-900 bg-white w-full p-8" style={{maxWidth: props.maxWidth}}>
-                {!!props.onClose && (
-                    <div className="flex items-center justify-end">
-                        <div className="flex p-2" onClick={props.onClose}>
-                            <div className="flex text-4xl text-gray-500 cursor-pointer">
-                                <CloseIcon />
-                            </div>
+            <div className="rounded-lg border border-neutral-200 bg-white w-full p-6 shadow-sm" style={{maxWidth: props.maxWidth}}>
+                <div className="flex items-center justify-between mb-3 select-none">
+                    <div className="flex items-center text-lg font-bold text-neutral-900">
+                        <span>{props.title}</span>
+                    </div>
+                    <div className="flex group" onClick={props.onClose}>
+                        <div className="flex text-2xl text-neutral-500 group-hover:text-neutral-900 cursor-pointer">
+                            <CloseIcon />
                         </div>
                     </div>
-                )}
+                </div>
                 {props.children}
             </div>          
         </div>
@@ -22,6 +23,7 @@ export const Modal = props => (
 );
 
 Modal.defaultProps = {
+    title: "",
     maxWidth: "500px",
     onClose: null,
 };
