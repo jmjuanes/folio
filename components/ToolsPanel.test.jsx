a918515a35ba7a17c1ce90c6a65cff1c02757e0c
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-26 18:15:34 +0000

    Fix tests

--- components/ToolsPanel.test.jsx
+++ components/ToolsPanel.test.jsx
@@ -2,8 +2,19 @@ import {render, screen} from "@testing-library/react";
 import {ToolsPanel} from "./ToolsPanel.jsx";
 
 jest.mock("@josemi-icons/react", () => ({
+    CircleIcon: jest.fn(() => "CIRCLE_ICON"),
+    EraseIcon: jest.fn(() => "ERASE_ICON"),
+    HandGrabIcon: jest.fn(() => "HAND_GRAB_ICON"),
+    ImageIcon: jest.fn(() => "IMAGE_ICON"),
+    LockIcon: jest.fn(() => "LOCK_ICON"),
     NoteIcon: jest.fn(() => "NOTE_ICON"),
+    PenIcon: jest.fn(() => "PEN_ICON"),
+    PointerIcon: jest.fn(() => "POINTER_ICON"),
+    SquareIcon: jest.fn(() => "SQUARE_ICON"),
+    TextIcon: jest.fn(() => "TEXT_ICON"),
     ToolsIcon: jest.fn(() => "TOOLS_ICON"),
+    TriangleIcon: jest.fn(() => "TRIANGLE_ICON"),
+    UnlockIcon: jest.fn(() => "UNLOCK_ICON"),
 }));
 
 jest.mock("../contexts/BoardContext.jsx", () => ({
