3d5d9005cf84c7990bc6aab8e2c311d267b9d877
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-28 18:53:16 +0000

    Rename home page as dashboard and integrate boards list component

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import {ExclamationTriangleIcon} from "@josemi-icons/react";
 import {DownloadIcon, DrawingIcon, ImageSlashIcon, renderIcon} from "@josemi-icons/react";
 import {DotsVerticalIcon, TrashIcon, CopyIcon} from "@josemi-icons/react";
-import {PrimaryButton} from "./Button.jsx";
-import {Dropdown, DropdownItem, DropdownSeparator} from "./Dropdown.jsx";
+import {loadFromJson, saveAsJson} from "../board/json.js";
+import {useClient} from "../contexts/ClientContext.jsx";
+import {useConfirm} from "../contexts/ConfirmContext.jsx";
+import {Dropdown, DropdownItem, DropdownSeparator} from "../components/Dropdown.jsx";
+import {PrimaryButton} from "../components/Button.jsx";
+import {useForceUpdate} from "../hooks/index.js";
 import {useDelay} from "../hooks/index.js";
 
 const BoardEmpty = props => (
@@ -93,7 +98,7 @@ const BoardActionButton = props => (
     </div>
 );
 
-export const BoardList = props => {
+const BoardList = props => {
     const [boards, setBoards] = React.useState(null);
     useDelay(props.delay, () => {
         return props.data().then(items => {
@@ -178,4 +183,84 @@ BoardList.defaultProps = {
     onBoardSave: null,
     onBoardDelete: null,
     onBoardDuplicate: null,
+};
+
+// Dashboard page
+export default props => {
+    const client = useClient();
+    const [updateKey, forceUpdate] = useForceUpdate();
+    const {showConfirm} = useConfirm();
+    return (
+        <div className="w-full">
+            <div className="mt-24 select-none">
+                <div className="font-crimson font-black text-9xl leading-none tracking-tight">
+                    <span>Folio.</span>
+                </div>
+            </div>
+            <div className="w-full mt-8">
+                <div className="rounded-md p-4 bg-white border border-neutral-200 flex gap-2 select-none">
+                    <div className="flex text-neutral-900 text-2xl animation-pulse">
+                        <ExclamationTriangleIcon />
+                    </div>
+                    <div className="">
+                        <div className="text-neutral-900 font-bold mb-2">Folio is still a Work in Progress</div>
+                        <div className="text-sm text-neutral-700">
+                            You might encounter occasional bugs or experience features that are still being refined. 
+                            We appreciate your patience and understanding as we work to deliver the best possible drawing experience.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-12">
+                <BoardList
+                    key={updateKey}
+                    title="Your boards"
+                    data={() => client.getUserBoards()}
+                    onCreate={props.onCreate}
+                    onLoad={() => {
+                        loadFromJson()
+                            .then(data => client.addBoard(data))
+                            .then(response => props.onOpen(response.id))
+                            .catch(error => console.error(error));
+                    }}
+                    onBoardClick={props.onOpen}
+                    onBoardSave={boardId => {
+                        client.getBoard(boardId)
+                            .then(data => saveAsJson(data))
+                            .catch(error => console.error(error));
+                    }}
+                    onBoardDelete={boardId => {
+                        showConfirm({
+                            title: "Delete board",
+                            message: "Are you sure? This action can not be undone.",
+                            callback: () => {
+                                client.deleteBoard(boardId)
+                                    .then(() => {
+                                        // TODO: display a confirmation message
+                                        forceUpdate();
+                                    })
+                                    .catch(error => console.error(error));
+                            },
+                        });
+                    }}
+                    onBoardDuplicate={boardId => {
+                        client.getBoard(boardId)
+                            .then(data => {
+                                return client.addBoard({
+                                    ...data,
+                                    title: `${data.title} - Copy`,
+                                    createdAt: Date.now(),
+                                    updatedAt: Date.now(),
+                                });
+                            })
+                            .then(() => {
+                                // TODO: display confirmation message
+                                forceUpdate();
+                            })
+                            .catch(error => console.error(error));
+                    }}
+                />
+            </div>
+        </div>
+    );
 };
