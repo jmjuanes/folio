b9ef67678dbb169dcdca3527321ee8d63737afd1
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-28 18:54:49 +0000

    Merge App into index.jsx

--- index.jsx
+++ index.jsx
@@ -1,18 +1,64 @@
 import React from "react";
+import Rouct from "rouct";
 import {createRoot} from "react-dom/client";
 import {RouterProvider} from "./contexts/RouterContext.jsx";
-import {ClientProvider} from "./contexts/ClientContext.jsx";
+import {ClientProvider, useClient} from "./contexts/ClientContext.jsx";
 import {ConfirmProvider} from "./contexts/ConfirmContext.jsx";
-import {App} from "./components/App.jsx";
+
+import Layout from "./layout.jsx";
+import DashboardPage from "./pages/dashboard.jsx";
+import BoardPage from "./pages/board.jsx";
 
 import "lowcss/dist/low.css";
+
+// Main app component
+const App = props => {
+    const client = useClient();
+    const handleOpen = id => {
+        return Rouct.redirect(`/board#id=${id}`);
+    };
+    const handleCreate = () => {
+        const boardData = {
+            title: "Untitled",
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+        };
+        client.addBoard(boardData)
+            .then(response => handleOpen(response.id))
+            .catch(error => console.error(error));
+    };
+    return (
+        <Rouct.Router pathPrefix={props.pathPrefix} routing={Rouct.BrowserRouting}>
+            <Rouct.Switch>
+                <Rouct.Route exact path="/board" render={request => (
+                    <BoardPage
+                        key={"board:" + request.query.id}
+                        id={request.query.id}
+                    />
+                )} />
+                <Rouct.Route exact path="*" render={() => (
+                    <Layout onCreate={handleCreate}>
+                        <Rouct.Switch>
+                            <Rouct.Route exact path="/dashboard" render={() => (
+                                <DashboardPage
+                                    onCreate={handleCreate}
+                                    onOpen={handleOpen}
+                                />
+                            )} />
+                        </Rouct.Switch>
+                    </Layout>
+                )} />
+            </Rouct.Switch>
+        </Rouct.Router>
+    );
+};
 
 // Mount app component
 createRoot(document.getElementById("root")).render((
     <RouterProvider>
         <ClientProvider>
             <ConfirmProvider>
-                <App />
+                <App pathPrefix="/" />
             </ConfirmProvider>
         </ClientProvider>
     </RouterProvider>
