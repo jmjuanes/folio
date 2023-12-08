b580c7c3f74abc8447e2b075477c00d0835e4122
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-12-08 09:11:01 +0000

    Migrate layout components

--- layout.jsx
+++ layout.jsx
@@ -15,16 +15,13 @@ const Layout = props => {
             <Navbar className="max-w-7xl">
                 <NavbarBrand href="./" >folio.</NavbarBrand>
                 <NavbarCollapse>
-                    <NavbarLink
-                        href="./dashboard"
-                        onClick={handleRedirect}
-                        text="Dashboard"
-                    />
+                    <NavbarLink href="./dashboard" onClick={handleRedirect}>
+                        <strong>Dashboard</strong>
+                    </NavbarLink>
                 </NavbarCollapse>
-                <NavbarAction
-                    text="Create Board"
-                    onClick={props.onCreate}
-                />
+                <NavbarAction onClick={props.onCreate}>
+                    <div className="font-bold">Create Board</div>
+                </NavbarAction>
             </Navbar>
             <Container className="max-w-6xl">
                 {props.children}
@@ -33,7 +30,7 @@ const Layout = props => {
                 <div className="mb-2 text-neutral-800">
                     <span><b>Folio</b> v{props.version}</span>
                 </div>
-                <div className="">
+                <div className="text-xs">
                     Designed by <FooterLink href="https://www.josemi.xyz" target="_blank">Josemi</FooterLink> in Valencia, Spain.
                 </div>
             </Footer>
