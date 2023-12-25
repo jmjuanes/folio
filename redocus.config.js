4bf5e0edfe4354312d78808804490f8928b906e9
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-12-25 23:26:08 +0100

    Update page wrapper

--- redocus.config.js
+++ redocus.config.js
@@ -10,9 +10,14 @@ const PageWrapper = props => (
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
             <meta name="title" content={props.site.title} />
             <meta name="description" content={props.site.description} />
+            <meta property="og:site_name" content={props.site.title} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={props.site.url} />
+            <meta property="og:image" content={props.site.url + "/og.png"}/>
+            <meta property="og:description" content={props.site.description} />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@700&display=swap" />
-            <link rel="stylesheet" href={"./low.css"} />
+            <link rel="stylesheet" href="./low.css" />
             <title>{`${props.page.data.title} - ${props.site.title}`}</title>
             <style>{`.font-plex {font-family: IBM Plex Serif, serif;}`}</style>
         </head>
@@ -34,6 +39,7 @@ module.exports = {
     siteMetadata: {
         title: "Folio",
         description: "Folio is a digital whiteboard for sketching and prototyping",
+        url: "https://",
     },
     pageComponents: {
         Icon: props => renderIcon(props.icon),
