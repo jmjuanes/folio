97a20d2a1479dc48b54e3d6b54c9e85310d22307
Josemi <josemijuanes@gmail.com>
2024-01-19 21:18:19 +0100

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
-const fs = require("node:fs");
 const path = require("node:path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const package = require("./package.json");
-
-const pages = fs.readdirSync(path.join(process.cwd(), "pages"), "utf8")
-    .filter(file => path.extname(file) === ".jsx")
-    .map(file => path.basename(file, ".jsx"));
 
 module.exports = {
     mode: process.env.NODE_ENV || "development", // "production",
@@ -84,18 +79,15 @@ module.exports = {
             chunkFilename: "[id].css",
         }),
         new webpack.DefinePlugin({
-            "process.env.PATH_PREFIX": JSON.stringify(process.env.PATH_PREFIX || "/"),
             "process.env.VERSION": JSON.stringify(package.version),
             "process.env.URL_REPOSITORY": JSON.stringify(package.repository),
             "process.env.URL_ISSUES": JSON.stringify(package.bugs),
             "process.env.URL_HOMEPAGE": JSON.stringify(package.homepage),
         }),
-        ...pages.map(page => {
-            return new HtmlWebpackPlugin({
-                template: path.join(__dirname, "index.html"),
-                filename: `${page}.html`,
-                minify: true,
-            });
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "index.html"),
+            filename: `app.html`,
+            minify: true,
         }),
     ],
 };
