a35f5bdbf711d175ed13962fbcabd0ce38481fd1
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-03-03 18:29:30 +0000

    chore: display features of folio in welcome dialog

--- components/dialogs/welcome.jsx
+++ components/dialogs/welcome.jsx
@@ -1,17 +1,41 @@
 import React from "react";
-import {DrawingIcon, FolderIcon, FileIcon} from "@josemi-icons/react";
+import {renderIcon, DrawingIcon, FolderIcon} from "@josemi-icons/react";
 import {Button, Centered, Overlay} from "@josemi-ui/react";
+
+const defaultWelcomeFeatures = [
+    {
+        icon: "shield-check",
+        title: "Privacy First.",
+        description: "Folio uses the local storage of your browser to ensure that your drawings are accessible only to you.",
+    },
+    {
+        icon: "drawing",
+        title: "Boundless Canvas.",
+        description: "Unleash your imagination on an infinite canvas for sketching, prototyping, and creating.",
+    },
+];
 
 export const WelcomeDialog = props => (
     <React.Fragment>
         <Overlay className="z-9" />
         <Centered className="fixed h-full z-10">
             <div className="w-full max-w-lg p-8 bg-white border border-neutral-200 rounded-lg shadow-sm">
-                <div className="pt-12 pb-8 select-none">
+                <div className="pt-12 pb-4 select-none">
                     <div className="font-black text-5xl mb-6 text-neutral-950 leading-none">Hello 👋</div>
                     <div className="">
                         Welcome to <b>folio</b>, our work-in-progress minimal digital whiteboard for sketching and prototyping.
                     </div>
+                </div>
+                <div className="flex gap-2 w-full pb-6">
+                    {(props.features || []).map(feature => (
+                        <div key={feature.title} className="rounded-md bg-neutral-50 text-neutral-700 p-4 w-full">
+                            <div className="text-3xl text-neutral-700 flex mb-1">
+                                {renderIcon(feature.icon)}
+                            </div>
+                            <div className="text-sm font-bold">{feature.title}</div>
+                            <div className="text-2xs">{feature.description}</div>
+                        </div>
+                    ))}
                 </div>
                 <div className="flex items-center gap-2">
                     <Button className="w-full" onClick={props.onCreate}>
@@ -34,3 +58,7 @@ export const WelcomeDialog = props => (
         </Centered>
     </React.Fragment>
 );
+
+WelcomeDialog.defaultProps = {
+    features: defaultWelcomeFeatures,
+};
