c1fe26b43fd7f513db68a3156ae01b303968571b
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-03-31 22:35:36 +0000

    Fix rendering bookmark component

--- components/elements/bookmark.jsx
+++ components/elements/bookmark.jsx
@@ -4,6 +4,7 @@ import {
     NONE,
     BOOKMARK_WIDTH,
     BOOKMARK_HEIGHT,
+    BOOKMARK_OFFSET,
 } from "../../lib/constants.js";
 import {useAssets} from "../../contexts/assets.jsx";
 
@@ -12,17 +13,30 @@ export const BookmarkElement = props => {
     const link = assets[props.assetId]?.data || {};
     const width = props.x2 - props.x1;
     const height = props.y2 - props.y1;
+    const style = {
+        // top: BOOKMARK_OFFSET + "px",
+        // left: BOOKMARK_OFFSET + "px",
+        height: height + "px",
+        width: width + "px",
+        transform: `translateX(${BOOKMARK_OFFSET}px) translateY(${BOOKMARK_OFFSET}px)`,
+        pointerEvents: "none",
+    };
     return (
         <g opacity={props.opacity}>
-            <foreignObject x={props.x1} y={props.y1} width={width} height={height}>
-                <div className="overflow-hidden rounded-md bg-white shadow-sm" style={{width, height}}>
+            <foreignObject
+                x={props.x1 - BOOKMARK_OFFSET}
+                y={props.y1 - BOOKMARK_OFFSET}
+                width={width + (2 * BOOKMARK_OFFSET)}
+                height={height + (2 * BOOKMARK_OFFSET)}
+            >
+                <div className="rounded-md bg-white shadow-sm" style={style}>
                     {link.src}
                 </div>
             </foreignObject>
             <rect
                 data-element={props.id}
                 x={props.x1}
-                y={props.y2}
+                y={props.y1}
                 width={Math.max(1, width)}
                 height={Math.max(1, height)}
                 rx="0"
