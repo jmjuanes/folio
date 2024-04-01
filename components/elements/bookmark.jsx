8f3751d5642122f09837c772f36c1b69fe66fe6d
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-04-01 17:45:38 +0000

    Improve rendering bookmarks and add bookmarks actions buttons

--- components/elements/bookmark.jsx
+++ components/elements/bookmark.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import {ImageIcon, renderIcon} from "@josemi-icons/react";
 import {
     TRANSPARENT,
     NONE,
     BOOKMARK_WIDTH,
-    BOOKMARK_HEIGHT,
     BOOKMARK_OFFSET,
 } from "../../lib/constants.js";
 import {useAssets} from "../../contexts/assets.jsx";
+import {copyTextToClipboard} from "../../lib/utils/clipboard.js";
+import {stopEventPropagation} from "../../lib/utils/events.js";
+
+// @private bookmark action button
+const BookmarkActionButton = props => (
+    <div onPointerDown={props.onPointerDown} className="cursor-pointer flex text-xl p-2 rounded-md border border-neutral-200 bg-white hover:bg-neutral-100">
+        {renderIcon(props.icon)}
+    </div>
+);
 
 export const BookmarkElement = props => {
     const assets = useAssets();
@@ -14,23 +23,44 @@ export const BookmarkElement = props => {
     const width = props.x2 - props.x1;
     const height = props.y2 - props.y1;
     const style = {
-        // top: BOOKMARK_OFFSET + "px",
-        // left: BOOKMARK_OFFSET + "px",
         height: height + "px",
         width: width + "px",
         transform: `translateX(${BOOKMARK_OFFSET}px) translateY(${BOOKMARK_OFFSET}px)`,
         pointerEvents: "none",
+        opacity: props.opacity,
     };
     return (
-        <g opacity={props.opacity}>
+        <g>
+            {props.selected && (
+                <foreignObject x={props.x1} y={props.y1 - 50} width={BOOKMARK_WIDTH} height={50}>
+                    <div className="flex justify-center gap-2" onPointerDown={stopEventPropagation}>
+                        <BookmarkActionButton
+                            icon="clipboard"
+                            onPointerDown={() => copyTextToClipboard(link.src)}
+                        />
+                        <a href={link.src} target="_blank" className="flex">
+                            <BookmarkActionButton icon="external-link" />
+                        </a>
+                    </div>
+                </foreignObject>
+            )}
             <foreignObject
                 x={props.x1 - BOOKMARK_OFFSET}
                 y={props.y1 - BOOKMARK_OFFSET}
                 width={width + (2 * BOOKMARK_OFFSET)}
                 height={height + (2 * BOOKMARK_OFFSET)}
             >
-                <div className="rounded-md bg-white shadow-sm" style={style}>
-                    {link.src}
+                <div className="select-none flex flex-col rounded-md bg-white shadow-sm overflow-hidden border border-neutral-200" style={style}>
+                    <div className="flex items-center justify-center bg-neutral-100 grow">
+                        <div className="flex text-5xl text-neutral-500">
+                            <ImageIcon />
+                        </div>
+                    </div>
+                    <div className="p-3 bg-white">
+                        <div className="text-xs w-full truncate">
+                            <strong>{link.src}</strong>
+                        </div>
+                    </div>
                 </div>
             </foreignObject>
             <rect
