71d651364c1cc2dd562c21a4a28c2606aa95a45c
Josemi <josemijuanes@gmail.com>
2024-05-30 18:42:57 +0200

    Fix image picker

--- app/components/form/index.jsx
+++ app/components/form/index.jsx
@@ -4,7 +4,6 @@ import {SquareIcon, CheckSquareIcon} from "@josemi-icons/react";
 import {FORM_OPTIONS} from "../../constants.js";
 import {ColorPicker} from "./color-picker.jsx";
 import {FontPicker} from "./font-picker.jsx";
-import {ImagePicker} from "./image-picker.jsx";
 
 // Tiny utility to check if a value is active
 export const checkIsActive = (value, currentValue, isActiveFn, data) => {
@@ -172,7 +171,24 @@ const optionTypes = {
         </div>
     ),
     [FORM_OPTIONS.IMAGE_SELECT]: props => (
-        <ImagePicker {...props} />
+        <div className={props.className || "grid grid-cols-5 gap-1 w-full"} style={props.style || {}}>
+            {(props.values || []).map(item => {
+                if (!checkIsVisible(item.value, props.value, props.isVisible, props.data)) {
+                    return null;
+                }
+                const active = checkIsActive(item.value, props.value, props.isActive, props.data);
+                const itemClass = classNames({
+                    "flex flex-col justify-center items-center rounded-md py-0 grow": true,
+                    "bg-neutral-900 text-white": active,
+                    "bg-neutral-100 hover:bg-neutral-200 cursor-pointer": !active,
+                });
+                return (
+                    <div key={item.value} className={itemClass} onClick={() => props.onChange(item.value)}>
+                        <img width="32px" height="32px" src={item.image} />
+                    </div>
+                );
+            })}
+        </div>
     ),
     [FORM_OPTIONS.SEPARATOR]: () => (
         <div className="w-full h-px bg-gray-500" />
