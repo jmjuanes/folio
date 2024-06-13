1202737ba8b853db983c262d59ec169276b5c6a2
Josemi <josemijuanes@gmail.com>
2024-06-13 19:43:54 +0200

    Display message about the backup tool in the welcome dialog

--- app/components/dialogs/welcome.jsx
+++ app/components/dialogs/welcome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {renderIcon, DrawingIcon, FolderIcon} from "@josemi-icons/react";
+import {renderIcon, DrawingIcon, FolderIcon, ExternalLinkIcon, ExclamationTriangleIcon} from "@josemi-icons/react";
 import {Button} from "../ui/button.jsx";
 import {Centered} from "../ui/centered.jsx";
 import {Overlay} from "../ui/overlay.jsx";
@@ -17,6 +17,30 @@ const defaultWelcomeFeatures = [
     },
 ];
 
+const RecoverMessage = () => (
+    <div className="mb-4">
+        <div className="w-full flex gap-2 p-3 rounded-md border border-neutral-200 bg-neutral-50">
+            <div className="shrink-0 flex text-xl animation-pulse">
+                <ExclamationTriangleIcon />
+            </div>
+            <div className="grow-1">
+                <div className="text-xs mb-2 leading-tight">
+                    <span>Comming from <b>www.josemi.xyz/folio</b>? </span>
+                    <span>You can use our <b>backup tool</b> to recover and reimport your board here.</span>
+                </div>
+                <div className="text-sm mb-1">
+                    <a href="https://www.josemi.xyz/folio-backup" target="_blank" className="inline-flex items-center gap-2 p-2 rounded-md border border-neutral-200 hover:bg-neutral-200">
+                        <div className="leading-none font-medium">Open backup tool</div>
+                        <div className="flex items-center">
+                            <ExternalLinkIcon />
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
 export const WelcomeDialog = props => (
     <React.Fragment>
         <Overlay className="z-50" />
@@ -28,9 +52,9 @@ export const WelcomeDialog = props => (
                         Welcome to <b>folio</b>, our work-in-progress minimal digital whiteboard for sketching and prototyping.
                     </div>
                 </div>
-                <div className="flex gap-2 w-full pb-6">
+                <div className="flex gap-2 w-full mb-6">
                     {(props.features || []).map(feature => (
-                        <div key={feature.title} className="rounded-md bg-neutral-50 text-neutral-700 p-4 w-full">
+                        <div key={feature.title} className="rounded-md bg-neutral-50 border border-neutral-200 text-neutral-700 p-4 w-full">
                             <div className="text-3xl text-neutral-700 flex mb-1">
                                 {renderIcon(feature.icon)}
                             </div>
@@ -39,7 +63,7 @@ export const WelcomeDialog = props => (
                         </div>
                     ))}
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 mb-6">
                     <Button className="w-full" onClick={props.onCreate}>
                         <div className="flex items-center text-lg">
                             <DrawingIcon />
@@ -53,7 +77,8 @@ export const WelcomeDialog = props => (
                         <div className="font-medium">Load from file</div>
                     </Button>
                 </div>
-                <div className="mt-8 text-neutral-400 text-center text-xs select-none">
+                <RecoverMessage />
+                <div className="text-neutral-400 text-center text-xs select-none">
                     <span><b>folio</b> v{process.env.VERSION}</span> 
                 </div>
             </div>
