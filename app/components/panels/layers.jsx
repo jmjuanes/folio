9ad7c9e25cd7faee325786e73916dace79c7b642
Josemi <josemijuanes@gmail.com>
2024-06-21 20:07:09 +0200

    Adding grouped layers

--- app/components/panels/layers.jsx
+++ app/components/panels/layers.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import classNames from "classnames";
 import {StackIcon, renderIcon, CheckIcon} from "@josemi-icons/react";
 import {useScene} from "../../contexts/scene.jsx";
 import {exportToDataURL} from "../../export.js";
 import {FIELDS, TRANSPARENT} from "../../constants.js";
-
 import transparentBg from "../../assets/transparent.svg";
+
+// Tiny hook to generate the preview of the element
+const useElementPreview = (elements, dependencies = []) => {
+    const [previewImage, setPreviewImage] = React.useState(null);
+    React.useEffect(() => {
+        // if (elements.length > 1 || !) {
+        //     const previewOptions = {
+        //         elements: elements,
+        //         width: 32,
+        //         height: 32,
+        //         background: TRANSPARENT,
+        //     };
+        //     exportToDataURL(previewOptions).then(image => {
+        //         setPreviewImage(image);
+        //     });
+        // }
+    }, dependencies);
+    return previewImage;
+};
 
 const previewStyle = {
     backgroundImage: `url('${transparentBg}')`,
@@ -20,29 +37,12 @@ const LayerItemAction = props => (
 );
 
 const LayerItem = props => {
-    const [previewImage, setPreviewImage] = React.useState(null);
-    // const classList = classNames({
-    //     "group flex justify-between p-2 rounded-lg cursor-pointer": true,
-    //     "hover:bg-neutral-100": !props.element.selected,
-    //     "bg-neutral-100": props.element.selected,
-    // });
-    React.useEffect(() => {
-        if (!props.element.creating) {
-            const previewOptions = {
-                elements: [props.element],
-                width: 32,
-                height: 32,
-                background: TRANSPARENT,
-            };
-            exportToDataURL(previewOptions).then(image => {
-                setPreviewImage(image);
-            });
-        }
-    }, [props.element[FIELDS.CREATING], props.element[FIELDS.EDITING], props.element[FIELDS.VERSION]]);
+    const element = props.element;
+    const previewImage = useElementPreview([element], [element[FIELDS.CREATING], element[FIELDS.EDITING], element[FIELDS.VERSION]]);
     return (
         <div className="group flex items-center gap-2 p-2 rounded-lg cursor-pointer hover:bg-neutral-100" onClick={props.onClick}>
             <div className="shrink-0 w-4">
-                {props.element.selected && (
+                {element.selected && (
                     <div className="flex items-center text-sm">
                         <CheckIcon />
                     </div> 
@@ -64,25 +64,56 @@ const LayerItem = props => {
     );
 };
 
+const LayerGroup = props => {
+
+};
+
 export const LayersPanel = props => {
     const scene = useScene();
+    const key = scene.page.elements.map(el => el.id + "." + (el.group || ".")).join("-");
+    const elementsTree = React.useMemo(() => {
+        const currentTree = [];
+        scene.page.elements.forEach(element => {
+            // Check if this element is inside a group
+            if (element.group) {
+                // Check if we need to initialize the group in the current tree
+                if (currentTree.length === 0 || currentTree[currentTree.length - 1]?.group !== element.group) {
+                    currentTree.push({group: element.group, elements: []});
+                }
+                // Add this element to the current tree
+                currentTree[currentTree.length - 1].elements.push(element);
+            }
+            // Other case, add as a single element
+            else {
+                currentTree.push(element);
+            }
+        });
+        return currentTree;
+    }, [key]);
     return (
         <div className="w-56 border border-neutral-200 rounded-xl shadow-md bg-white p-2 overflow-y-auto" style={{maxHeight:"calc(100vh - 5rem)"}}>
             <div className="flex flex-col-reverse gap-1">
-                {scene.page.elements.map(element => (
-                    <LayerItem
-                        key={element.id}
-                        element={element}
-                        onClick={() => props?.onElementSelect(element)}
-                        onDeleteClick={event => {
-                            event.stopPropagation();
-                            props?.onElementDelete(element);
-                        }}
-                        onDuplicateClick={event => {
-                            event.stopPropagation();
-                            props?.onElementDuplicate(element);
-                        }}
-                    />
+                {elementsTree.map(element => (
+                    <React.Fragment>
+                        {element.group && element.elements?.length > 0 && (
+                            <div>Group</div>
+                        )}
+                        {!element.group && (
+                            <LayerItem
+                                key={element.id}
+                                element={element}
+                                onClick={() => props?.onElementSelect(element)}
+                                onDeleteClick={event => {
+                                    event.stopPropagation();
+                                    props?.onElementDelete(element);
+                                }}
+                                onDuplicateClick={event => {
+                                    event.stopPropagation();
+                                    props?.onElementDuplicate(element);
+                                }}
+                            />
+                        )}
+                    </React.Fragment>
                 ))}
                 {scene.page.elements.length === 0 && (
                     <div className="flex flex-col items-center justify-center p-3 border border-dashed border-neutral-300 rounded-lg">
