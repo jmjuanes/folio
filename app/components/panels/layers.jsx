991d0f2e1b62b7268877698d5a338037d9788859
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-06-30 17:19:29 +0000

    Allow to rename layers

--- app/components/panels/layers.jsx
+++ app/components/panels/layers.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {useUpdate} from "react-use";
+import classNames from "classnames";
 import {StackIcon, renderIcon, CheckIcon, ChevronDownIcon, ChevronRightIcon} from "@josemi-icons/react";
 import {useScene} from "../../contexts/scene.jsx";
 import {exportToDataURL} from "../../export.js";
 import {FIELDS, TRANSPARENT} from "../../constants.js";
 import transparentBg from "../../assets/transparent.svg";
-import classNames from "classnames";
 
 // Tiny hook to generate the preview of the element
 const useElementPreview = (elements, dependencies = []) => {
@@ -39,8 +39,15 @@ const LayerItemAction = props => (
 );
 
 const LayerItem = props => {
+    const inputRef = React.useRef(null);
     const element = props.element;
     const previewImage = useElementPreview([element], [element[FIELDS.CREATING], element[FIELDS.EDITING], element[FIELDS.VERSION]]);
+    // Autofocus to input when layer is on editing mode
+    React.useEffect(() => {
+        if (props.editing && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [props.editing]);
     return (
         <div className="group flex items-center gap-2 p-2 rounded-lg cursor-pointer hover:bg-neutral-100" onClick={props.onClick}>
             <div className="shrink-0 w-4">
@@ -55,13 +62,48 @@ const LayerItem = props => {
                     <img src={previewImage} width="100%" height="100%" />
                 )}
             </div>
-            <div className="flex items-center grow">
-                <span className="text-xs font-medium text-neutral-700">{props.element[FIELDS.NAME] || "Layer"}</span>
-            </div>
-            <div className="flex flex-row-reverse shrink-0 gap-1 items-center group-hover:opacity-100 opacity-0">
-                <LayerItemAction icon="trash" onClick={props.onDeleteClick} />
-                <LayerItemAction icon="copy" onClick={props.onDuplicateClick} />
-            </div>
+            {!props.editing && (
+                <React.Fragment>
+                    <div className="flex items-center grow">
+                        <div className="w-20 text-xs font-medium text-neutral-700 truncate" title={props.element[FIELDS.NAME]}>
+                            <span>{props.element[FIELDS.NAME] || "Layer"}</span>
+                        </div>
+                    </div>
+                    <div className="flex flex-row-reverse shrink-0 gap-1 items-center group-hover:opacity-100 opacity-0">
+                        <LayerItemAction icon="trash" onClick={props.onDeleteClick} />
+                        <LayerItemAction icon="copy" onClick={props.onDuplicateClick} />
+                        <LayerItemAction icon="pencil" onClick={props.onEditClick} />
+                    </div>
+                </React.Fragment>
+            )}
+            {props.editing && (
+                <React.Fragment>
+                    <div className="flex items-center grow">
+                        <input
+                            ref={inputRef}
+                            className="w-full bg-transparent border-none outline-none p-0 text-xs"
+                            defaultValue={props.element[FIELDS.NAME] || "Layer"}
+                            onKeyUp={event => {
+                                // Check for enter key --> submit new page title
+                                if (event.key === "Enter") {
+                                    return props.onEditSubmit(event, inputRef.current.value);
+                                }
+                                // Check for ESC key --> Cancel editing page
+                                else if (event.key === "Escape") {
+                                    return props.onEditCancel(event);
+                                }
+                            }}
+                        />
+                    </div>
+                    <div className="flex flex-row-reverse shrink-0 gap-1 items-center">
+                        <LayerItemAction icon="close" onClick={props.onEditCancel} />
+                        <LayerItemAction
+                            icon="check"
+                            onClick={event => props.onEditSubmit(event, inputRef?.current?.value || "")}
+                        />
+                    </div>
+                </React.Fragment>
+            )}
         </div>
     );
 };
@@ -86,6 +128,7 @@ const LayerGroupItem = props => {
 };
 
 export const LayersPanel = props => {
+    const [editingLayer, setEditingLayer] = React.useState("");
     const expandedGroups = React.useRef(new Set());
     const scene = useScene();
     const update = useUpdate();
@@ -122,11 +165,12 @@ export const LayersPanel = props => {
         // 2. Trigger an update if we have added at least one group
         // in the list of expanded groups
         if (expandedGroups.current.size > 0 && shouldUpdate) {
+            // setEditingLayer("");
             update();
         }
     }, [scene.page.activeGroup]);
     return (
-        <div className="w-56 border border-neutral-200 rounded-xl shadow-md bg-white p-2 overflow-y-auto" style={{maxHeight:"calc(100vh - 8rem)"}}>
+        <div className="w-64 border border-neutral-200 rounded-xl shadow-md bg-white p-2 overflow-y-auto" style={{maxHeight:"calc(100vh - 8rem)"}}>
             <div className="flex flex-col-reverse gap-0">
                 {scene.page.elements.map(element => (
                     <React.Fragment key={element.id + "." + (element.group || "")}>
@@ -135,14 +179,36 @@ export const LayersPanel = props => {
                                 <LayerItem
                                     key={element.id}
                                     element={element}
-                                    onClick={() => props?.onElementSelect(element)}
+                                    editing={editingLayer === element.id}
+                                    onClick={() => {
+                                        if (editingLayer !== element.id) {
+                                            props?.onElementSelect(element);
+                                            setEditingLayer("");
+                                        }
+                                    }}
                                     onDeleteClick={event => {
                                         event.stopPropagation();
+                                        setEditingLayer("");
                                         props?.onElementDelete(element);
                                     }}
                                     onDuplicateClick={event => {
                                         event.stopPropagation();
+                                        setEditingLayer("");
                                         props?.onElementDuplicate(element);
+                                    }}
+                                    onEditClick={event => {
+                                        event.stopPropagation();
+                                        setEditingLayer(element.id);
+                                    }}
+                                    onEditSubmit={(event, value) => {
+                                        event.stopPropagation();
+                                        element[FIELDS.NAME] = value || element[FIELDS.NAME];
+                                        setEditingLayer("");
+                                        props?.onElementRename(element);
+                                    }}
+                                    onEditCancel={event => {
+                                        event.stopPropagation();
+                                        setEditingLayer("");
                                     }}
                                 />
                             </div>
@@ -159,6 +225,7 @@ export const LayersPanel = props => {
                                     else {
                                         expandedGroups.current.add(element.group);
                                     }
+                                    setEditingLayer("");
                                     update();
                                 }}
                             />
