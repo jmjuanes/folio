abff1736e6b8cc72e20921fa527a4a550bfe6a08
Josemi <josemijuanes@gmail.com>
2024-09-26 20:36:41 +0200

    Initialize default theme

---
+++ app/themes/default.js
@@ -0,0 +1,11 @@
+export default {
+    "editor": "text-neutral-900",
+    "island": "shadow-sm bg-white border border-neutral-200",
+    "island.separator": "bg-neutral-200",
+    "island.item": "hover:bg-neutral-100 text-neutral-800",
+    "island.item.active": "bg-neutral-100 text-neutral-950",
+
+    "context-menu": "bg-white shadow-sm border border-neutral-200",
+    "context-menu.item": "hover:bg-neutral-200",
+    "context-menu.separator": "bg-neutral-200",
+};
