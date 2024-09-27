1dd97cdc55214c55f69d40c1a073374b62f9497b
Josemi <josemijuanes@gmail.com>
2024-09-27 18:34:50 +0200

    Remove unused import from button component

--- app/components/ui/button.jsx
+++ app/components/ui/button.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import classNames from "classnames";
 import {themed} from "../../contexts/theme.jsx";
 
 export const Button = ({className, disabled, variant = "primary", ...props}) => (
