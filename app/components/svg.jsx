b6af2e98e6e2d79adfe6d62c7d427683d98fd9e7
Josemi <josemijuanes@gmail.com>
2024-10-03 19:22:25 +0200

    Refactor svg container component

--- app/components/svg.jsx
+++ app/components/svg.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 
-export const SvgContainer = props => (
-    <svg width={props.width} height={props.height} style={props.style}>
-        {props.children}
+const defaultStyle = {
+    position: "absolute",
+    overflow: "visible",
+    top: "-1px",
+    left: "-1px",
+};
+
+export const SvgContainer = ({width, height, style, children}) => (
+    <svg width={width ?? "1px"} height={height ?? "1px"} style={{...defaultStyle, ...style}}>
+        {children}
     </svg>
 );
-
-SvgContainer.defaultProps = {
-    width: "1px",
-    height: "1px",
-    style: {
-        position: "absolute",
-        overflow: "visible",
-        top: "-1px",
-        left: "-1px",
-    },
-};
