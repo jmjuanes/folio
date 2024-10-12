46e2ca14a0b100ebdd3949118874b019dc620791
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-10-12 15:42:28 +0000

    Fix copying illustration assets

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,17 +1,10 @@
 const fs = require("node:fs");
 const path = require("node:path");
+const {globSync} = require("glob");
 
-const rootPath = path.resolve(__dirname, "../");
-const buildPath = path.join(rootPath, "www/");
+// const rootPath = path.resolve(__dirname, "../");
+const buildPath = path.join(process.cwd(), "www/");
 const assets = [
-    // {
-    //     from: "public/index.html",
-    //     to: "./index.html",
-    // },
-    // {
-    //     from: "public/privacy.html",
-    //     to: "./privacy.html",
-    // },
     {
         from: "public/404.html",
         to: "./404.html",
@@ -32,15 +25,18 @@ const assets = [
         from: "public/og.png",
         to: "./og.png",
     },
+    {
+        from: "public/illustration-*.png",
+    },
 ];
 
 assets.forEach(asset => {
-    if (asset) {
-        const srcPath = path.resolve(rootPath, asset.from);
-        const dstPath = path.resolve(buildPath, asset.to);
-
-        console.log(`${srcPath} => ${dstPath}`);
-        // Note: cpSync is still experimental
-        fs.cpSync(srcPath, dstPath);
+    if (asset && asset?.from) {
+        return globSync(asset.from).forEach(file => {
+            const src = path.resolve(process.cwd(), file);
+            const dest = path.resolve(buildPath, asset.to || path.basename(file));
+            console.log(`[COPY] ${src} => ${dest}`);
+            fs.cpSync(src, dest);
+        });
     }
 });
