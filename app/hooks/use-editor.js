d469789a758c5b815e63a8ea31921158022c7e17
Josemi <josemijuanes@gmail.com>
2024-10-16 20:58:28 +0200

    Add libraries to editor

--- app/hooks/use-editor.js
+++ app/hooks/use-editor.js
@@ -24,6 +24,7 @@ import {
     getElementSnappingPoints,
 } from "../elements.js";
 import {useScene} from "../contexts/scene.jsx";
+import {useLibraries} from "../contexts/libraries.jsx";
 
 // @private create a new editor state
 const createInitialEditorState = (props, scene) => {
@@ -52,6 +53,8 @@ const createInitialEditorState = (props, scene) => {
         exportVisible: false,
         pagesVisible: false,
         layersVisible: false,
+        librariesVisible: false,
+        libraryCreateVisible: false,
 
         // @description state for welcome items
         hintsVisible: props.showHints && isSceneEmpty,
@@ -64,11 +67,14 @@ const createInitialEditorState = (props, scene) => {
 export const useEditor = props => {
     const update = useUpdate();
     const scene = useScene();
+    const libraries = useLibraries();
     const editor = React.useRef(null);
     const onChangeRef = React.useRef(props?.onChange);
+    const onChangeLibrariesRef = React.useRef(props?.onChangeLibraries);
 
     // We need to update the reference to the onChange function
     onChangeRef.current = props?.onChange;
+    onChangeLibrariesRef.current = props?.onChangeLibraries;
 
     // Initialize editor state
     if (!editor.current) {
@@ -77,6 +83,13 @@ export const useEditor = props => {
         // @description dispatch an editor change
         const dispatchChange = () => {
             return onChangeRef.current(scene.toJSON());
+        };
+
+        // @description dispatch a library change
+        const dispatchLibraryChange = () => {
+            if (typeof onChangeLibrariesRef.current === "function") {
+                return onChangeLibrariesRef.current(libraries.getAll());
+            }
         };
 
         // @private get position based on the grid state
@@ -693,6 +706,7 @@ export const useEditor = props => {
             update: update,
             state: editorState,
             dispatchChange: dispatchChange,
+            dispatchLibraryChange: dispatchLibraryChange,
             events: editorEvents,
         };
     }
