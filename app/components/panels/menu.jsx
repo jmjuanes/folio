971a987428bc6f30c676e2bd998c1a1fdff920c1
Josemi <josemijuanes@gmail.com>
2025-02-28 20:40:51 +0100

    Restore menu item to clear current page

--- app/components/panels/menu.jsx
+++ app/components/panels/menu.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import {Island} from "../ui/island.jsx";
 import {useEditorComponents} from "../../contexts/editor-components.jsx";
+import {useEditor} from "../../contexts/editor.jsx";
+import {useConfirm} from "../../contexts/confirm.jsx";
 
 // @description: default menu panel
 export const Menu = () => {
+    const editor = useEditor();
+    const {showConfirm} = useConfirm();
     const {EditorMenu, PagesMenu, SettingsMenu} = useEditorComponents();
+
+    // callback to handle clearing the current page
+    const handlePageClear = React.useCallback(() => {
+        return showConfirm({
+            title: "Clear Page",
+            message: "This will remove all elements of this page. Do you want to continue?",
+            confirmText: "Yes, clear page",
+            callback: () => {
+                editor.clearPage(editor.page.id);
+                editor.dispatchChange();
+                editor.update();
+            },
+        });
+    }, [editor, showConfirm, editor.page.id]);
 
     return (
         <Island>
             {!!EditorMenu && <EditorMenu />}
             {!!PagesMenu && <PagesMenu />}
             {!!SettingsMenu && <SettingsMenu />}
+            <Island.Button
+                icon="trash"
+                onClick={handlePageClear}
+                disabled={editor.page.readonly}
+            />
         </Island>
     );
 };
