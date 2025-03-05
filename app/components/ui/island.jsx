1582e96520defb8fa931f48df0549a7b26d2d2a4
Josemi <josemijuanes@gmail.com>
2025-03-05 19:02:11 +0100

    Improve island styles

--- app/components/ui/island.jsx
+++ app/components/ui/island.jsx
@@ -1,24 +1,39 @@
 import React from "react";
+import classNames from "classnames";
 import {renderIcon, ChevronDownIcon} from "@josemi-icons/react";
-import {themed} from "../../contexts/theme.jsx";
 
+// @description a container for grouping elements in the editor layout
+// @param {object} props React props
+// @param {React.ReactNode} props.children React children
+// @param {string} props.className additional class name
 export const Island = props => (
-    <div className={themed("flex gap-1 p-1 rounded-xl", "island", props.className)}>
+    <div className={classNames("flex gap-1 p-1 rounded-xl shadow-sm bg-white border-neutral-200", props.className)}>
         {props.children}
     </div>
 );
 
+// @description a vertical separator for the Island component
 Island.Separator = () => (
-    <div className={themed("w-px", "island.separator")} />
+    <div className="w-px bg-neutral-200" />
 );
 
+// @description a button for the Island component
+// @param {object} props React props
+// @param {React.ReactNode} props.children React children
+// @param {string} props.className additional class name
+// @param {string} props.icon icon name
+// @param {string} props.text button text
+// @param {boolean} props.disabled is the button disabled
+// @param {boolean} props.active is the button active
+// @param {boolean} props.showChevron should the button show a chevron
+// @param {function} props.onClick button click handler
 Island.Button = props => {
-    const classList = themed({
+    const classList = classNames({
         "flex justify-between items-center gap-1 p-2 rounded-lg": true,
-        "cursor-pointer": !props.disabled && !props.active,
+        "cursor-pointer": !props.disabled,
         "cursor-not-allowed opacity-40 pointer-events-none": props.disabled,
-        "island.item": !props.active,
-        "island.item.active": !props.disabled && props.active,
+        "hover:bg-neutral-200 text-neutral-900 group-focus-within:bg-neutral-950 group-focus-within:text-white": !props.active,
+        "bg-neutral-950 text-white": !props.disabled && props.active,
     }, props.className);
     const handleClick = () => {
         return !props.disabled && props?.onClick?.();
