4be50a77f18cf67149decda13bbaad4307383551
Josemi <josemijuanes@gmail.com>
2025-03-21 20:47:54 +0100

    Move some of the app stuff to folio-react folder

---
+++ folio-react/README.md
@@ -0,0 +1,2 @@
+# folio-react
+
---
+++ folio-react/index.jsx
@@ -0,0 +1,5 @@
+export {VERSION} from "./constants.js";
+
+export {createMemoryStore} from "./store/memory.js";
+
+export {Editor as FolioEditor} from "./components/editor.jsx";
