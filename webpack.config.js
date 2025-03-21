75e621a500b5da78a53888b55d80b670fbc7dfe9
Josemi <josemijuanes@gmail.com>
2025-03-21 20:49:02 +0100

    Update webpack configuration for new build of folio

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,48 @@
-import fs from "node:fs";
 import path from "node:path";
 import webpack from "webpack";
-import CopyWebpackPlugin from "copy-webpack-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-// import env from "./server/utils/environment.js";
-
-// read package.json
-const pkg = JSON.parse(fs.readFileSync("./package.json"));
-
-// const CLIENTS = {
-//     LOCAL: path.join(__dirname, "app/clients/local.js"),
-//     REMOTE: path.join(__dirname, "app/clients/remote.js"),
-// };
+import TerserPlugin from "terser-webpack-plugin";
+// import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import pkg from "./package.json" with {type: "json"};
 
 export default {
-    mode: process.env.NODE_ENV || "development", // "production",
+    mode: process.env.NODE_ENV || "development",
     target: "web",
-    entry: path.resolve("app/index.jsx"),
-    output: {
-        path: path.resolve("www"),
-        publicPath: "./",
-        filename: "[name].[contenthash].js",
-        chunkFilename: "[name].[contenthash].chunk.js",
-        assetModuleFilename: "assets/[hash][ext][query]",
-    },
-    resolve:{
-        alias: {
-            // "@folio/client": CLIENTS[(env?.CLIENT || "LOCAL").toUpperCase()],
-            "@folio/client": path.resolve("app/clients/local.js"),
+    entry: {
+        "folio-react": {
+            import: path.resolve("folio-react/index.jsx"),
+            filename: "folio-react/folio-react.js",
+            layer: "folio-react",
         },
+    },
+    output: {
+        path: path.resolve("dist"),
+        publicPath: "./",
+        clean: false,
+        library: {
+            type: "module",
+        },
+        // filename: "[name].[contenthash].js",
+        // chunkFilename: "[name].[contenthash].chunk.js",
+        // assetModuleFilename: "assets/[hash][ext][query]",
     },
     optimization: {
-        splitChunks: {
-            chunks: "all",
-        },
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
     },
-    devServer: {
-        hot: false,
-        static: {
-            directory: path.resolve("www"),
-            staticOptions: {
-                extensions: ["html"],
+    experiments: {
+        layers: true,
+        outputModule: true,
+    },
+    externals: {
+        byLayer: {
+            "folio-react": {
+                "react": "React",
+                "react-dom": "ReactDOM",
             },
-        },
-        historyApiFallback: {
-            rewrites: [
-                {from: /^\/$/, to: "app.html"},
-                {from: /^\/index.html$/, to: "app.html"},
-            ],
-        },
-        devMiddleware: {
-            writeToDisk: true,
         },
     },
     module: {
@@ -58,7 +50,7 @@ export default {
             {
                 test: /\.(js|jsx)$/,
                 include: [
-                    path.resolve("app"),
+                    path.resolve("folio-react"),
                 ],
                 exclude: /(node_modules|www)/,
                 loader: "babel-loader",
@@ -75,12 +67,12 @@ export default {
             },
             {
                 test: /\.(png|jpg|jpeg|svg)$/,
-                type: "asset/resource",
+                type: "asset/inline",
             },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                type: "asset/resource",
-            },
+            // {
+            //     test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            //     type: "asset/inline",
+            // },
             {
                 test: /resources\/stickers\/([\w-]+)\.svg$/,
                 type: "asset/inline",
@@ -90,24 +82,10 @@ export default {
     plugins: [
         new webpack.ProgressPlugin(),
         new webpack.DefinePlugin({
-            // Global values
             "process.env.VERSION": JSON.stringify(pkg.version),
             "process.env.URL_REPOSITORY": JSON.stringify(pkg.repository),
             "process.env.URL_ISSUES": JSON.stringify(pkg.bugs),
             "process.env.URL_HOMEPAGE": JSON.stringify(pkg.homepage),
-            // ENV values
-            // "process.env.APP": JSON.stringify(env.APP || "LITE"),
-            // "process.env.CLIENT": JSON.stringify(env.CLIENT || "LOCAL"),
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                path.resolve("node_modules/lowcss/low.css"),
-            ],
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve("app/template.html"),
-            filename: "app.html",
-            minify: true,
         }),
     ],
 };
