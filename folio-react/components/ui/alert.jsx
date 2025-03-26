8ad7885981a805271b7d781c6bc2304f92ec49d8
Josemi <josemijuanes@gmail.com>
2025-03-26 20:15:22 +0100

    Display a warning alert in keyboard shortcuts dialog if they are disabled

--- folio-react/components/ui/alert.jsx
+++ folio-react/components/ui/alert.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-// import {themed} from "../../contexts/theme.jsx";
+import classNames from "classnames";
 import {renderIcon} from "@josemi-icons/react";
 
-export const Alert = props => {
-    // const alertClassName = themed({});
+export const Alert = ({className, variant = "", icon = "", text = "", children, ...props}) => {
+    const alertClassName = classNames({
+        "rounded-xl shadow-sm border p-3 flex gap-2 items-center": true,
+        "bg-yellow-100 text-yellow-900 border-yellow-200": variant === "warning",
+    }, className);
+
     return (
-        <div className="rounded-xl shadow-sm border border-yellow-200 bg-yellow-100 text-yellow-900 p-2 flex gap-2 items-center">
-            {props.icon && (
-                <div className="flex items-center p-0">
-                    {renderIcon(props.icon)}
+        <div className={alertClassName} {...props}>
+            {icon && (
+                <div className="flex items-center text-lg">
+                    {renderIcon(icon)}
                 </div>
             )}
-            <div className="text-sm">{props.children || props.text}</div>
+            <div className="text-sm leading-none">{children || text}</div>
         </div>
     );
 };
