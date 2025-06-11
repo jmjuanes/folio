19f6ef8cbd85a4d715f24ce31fc7a9dcb2b00fb1
Josemi <josemijuanes@gmail.com>
2025-06-11 18:51:51 +0200

    Fix generating tokens

--- server/utils/token.js
+++ server/utils/token.js
@@ -8,21 +8,21 @@ const generateToken = (size = 32) => {
 };
 
 // Get JWT secret from environment or use a default
-const JWT_SECRET = environment.JWT_SECRET || generateToken(64);
-const TOKEN_EXPIRATION = environment.JWT_EXPIRATION || "7d"; // JWT tokens will expire after 7 days
+const TOKEN_SECRET = environment.TOKEN_SECRET || generateToken(64);
+const TOKEN_EXPIRATION = environment.TOKEN_EXPIRATION || "7d";
 
 // The access token is generated once when the server starts
 export const ACCESS_TOKEN = environment.ACCESS_TOKEN || generateToken();
 
 // Generate a JWT token for API access after authentication
 export const generateJwtToken = () => {
-    return jwt.sign({authenticated: true}, JWT_SECRET, {expiresIn: TOKEN_EXPIRATION});
+    return jwt.sign({authenticated: true}, TOKEN_SECRET, {expiresIn: TOKEN_EXPIRATION});
 };
 
 // verify the provided JWT token
 export const verifyJwtToken = token => {
     try {
-        return jwt.verify(token, JWT_SECRET);
+        return jwt.verify(token, TOKEN_SECRET);
     }
     catch (error) {
         return null;
