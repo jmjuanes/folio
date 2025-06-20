1f2a000ee43f77e900e64fd48e9dc7a683665889
Josemi <josemijuanes@gmail.com>
2025-06-20 19:03:27 +0200

    Restore router

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import {ConfirmProvider} from "folio-react/contexts/confirm.jsx";
 import {App} from "./components/app.jsx";
 import {ClientProvider} from "./contexts/client.jsx";
 import {AuthenticationProvider} from "./contexts/authentication.jsx";
+import {RouterProvider} from "./contexts/router.jsx";
 
 createRoot(document.getElementById("root")).render((
-    <ClientProvider>
-        <AuthenticationProvider>
-            <div>Hello world</div>
-        </AuthenticationProvider>
-    </ClientProvider>
+    <RouterProvider>
+        <ClientProvider>
+            <AuthenticationProvider>
+                <ConfirmProvider>
+                    <App />
+                </ConfirmProvider>
+            </AuthenticationProvider>
+        </ClientProvider>
+    </RouterProvider>
 ));
