4dd22287159c079a9c54e7bd96e3d12813f36bff
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-06-24 21:29:30 +0000

    Improve hook to manage form data

--- folio-react/hooks/use-form-data.js
+++ folio-react/hooks/use-form-data.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-// @private internal form data reducer method
-const formDataReducer = (state, action) => {
-    return Object.assign({}, state, {
-        [action.key]: action.value,
-    });
-};
-
 // @description allows to create and manage form data
-export const useFormData = initialData => {
-    const [data, dataDispatcher] = React.useReducer(formDataReducer, initialData);
-    const dispatcher = React.useCallback((key, value) => {
-        dataDispatcher({key, value});
-    }, []);
-    return [data, dispatcher];
+export const useFormData = (initialData = {}) => {
+    const [data, setData] = React.useState(initialData);
+
+    // method to update a single field in the form data
+    const setKeyValue = React.useCallback((key, value) => {
+        return setData(prevData => {
+            return Object.assign({}, prevData, {
+                [key]: value,
+            });
+        });
+    }, [setData]);
+
+    // return the data, the setKeyValue method, and the setData method
+    return [data, setKeyValue, setData];
 };
