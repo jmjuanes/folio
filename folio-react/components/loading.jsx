e8e812886b2d1957a7ca81f7bd729b05d212eeef
Josemi <josemijuanes@gmail.com>
2025-07-01 18:07:51 +0200

    Add loading spinner for loading component view

--- folio-react/components/loading.jsx
+++ folio-react/components/loading.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import {DrawingIcon} from "@josemi-icons/react";
-import {Centered} from "./ui/centered.jsx";
+import { Centered } from "./ui/centered.jsx";
 
-export const Loading = () => (
-    <Centered>
-        <div className="flex items-center text-4xl text-gray-900">
-            <DrawingIcon />
+export const LoadingSpinner = () => (
+    <div className="animation-spin h-6 w-6 relative">
+        <div className="absolute top-0 left-0 w-full h-full rounded-full border-current border-4 border-solid opacity-20" />
+        <div
+            className="absolute top-0 left-0 w-full h-full rounded-full border-transparent border-4 border-solid opacity-90"
+            style={{
+                borderLeftColor: "currentColor",
+            }}
+        />
+    </div>
+);
+
+export const Loading = props => (
+    <Centered className={props.className} style={props.style}>
+        <div className="flex items-center">
+            <LoadingSpinner />
         </div>
     </Centered>
 );
