197f17778f11b549b536ec5b4ceef5b48502b2de
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-07-06 11:38:35 +0200

    Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ export default {
     transform: {
         "^.+\\.(js|jsx)$": "babel-jest",
     },
+    extensionsToTreatAsEsm: [".js", ".jsx"],
     moduleNameMapper: {
         "^@lib(.*)$": "<rootDir>/lib$1",
         "^@components(.*)$": "<rootDir>/components$1",
