731d9447ea81f9648a19ddc2226c51d21efa10aa
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-07-06 18:31:24 +0200

    Update color picker tests

--- folio-react/components/form/__tests__/color-picker.test.jsx
+++ folio-react/components/form/__tests__/color-picker.test.jsx
@@ -6,19 +6,29 @@ import "@testing-library/jest-dom";
 import { ColorPicker } from "../color-picker.jsx";
 
 describe("<ColorPicker />", () => {
+    const colors = ["#fff", "#000", "#eaea"]
+
     it("should render", () => {
         render(<ColorPicker />);
         expect(screen.getByTestId("colorpicker")).toBeInTheDocument();
     });
 
     it("should display color values", () => {
-        const colors = ["#fff", "#000"];
         render(<ColorPicker values={colors} />);
-
         screen.getAllByTestId("colorpicker-value").forEach((element, index) => {
             expect(element).toHaveStyle({
                 backgroundColor: colors[index],
             });
         });
+    });
+
+    it("should emit 'onChange' when clicking on an color value of the palette", () => {
+        const handleChange = jest.fn();
+        const colorIndex = 1;
+        const user = userEvent.setup();
+        render(<ColorPicker values={colors} onChange={handleChange} />);
+
+        user.click(screen.getAllByTestId("colorpicker-value")[colorIndex]);
+        expect(handleChange).toHaveBeenCalledWith(colors[colorIndex]);
     });
 });
