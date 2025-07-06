13a8a495a22bc529580ca35b9c456291f635fbb4
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-07-06 16:56:12 +0000

    Update color pciker tests

--- folio-react/components/form/__tests__/color-picker.test.jsx
+++ folio-react/components/form/__tests__/color-picker.test.jsx
@@ -1,4 +1,4 @@
-import { jest } from "@jest/globals";
+import { expect, jest } from "@jest/globals";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
@@ -13,6 +13,15 @@ describe("<ColorPicker />", () => {
         expect(screen.getByTestId("colorpicker")).toBeInTheDocument();
     });
 
+    it("should display color provided as value", () => {
+        const color = "#ff0000";
+        render(<ColorPicker value={color} />);
+        expect(screen.getByTestId("colorpicker-preview")).toHaveStyle({
+            backgroundColor: color,
+        });
+        expect(screen.getByTestId("colorpicker-input")).toHaveValue(color);
+    });
+
     it("should display color values", () => {
         render(<ColorPicker values={colors} />);
         screen.getAllByTestId("colorpicker-value").forEach((element, index) => {
@@ -20,6 +29,15 @@ describe("<ColorPicker />", () => {
                 backgroundColor: colors[index],
             });
         });
+    });
+
+    it("should change input value when clicking on a color value of the palette", async () => {
+        const user = userEvent.setup();
+        render(<ColorPicker values={colors} />);
+
+        await user.click(screen.getAllByTestId("colorpicker-value")[0]);
+
+        expect(screen.getByTestId("colorpicker-input")).toHaveValue(colors[0]);
     });
 
     it("should emit 'onChange' when clicking on an color value of the palette", async () => {
@@ -31,5 +49,15 @@ describe("<ColorPicker />", () => {
         await user.click(screen.getAllByTestId("colorpicker-value")[colorIndex]);
 
         expect(handleChange).toHaveBeenCalledWith(colors[colorIndex]);
+    });
+
+    it("should emit 'onChange' when typing a color value in the input", async () => {
+        const handleChange = jest.fn();
+        const user = userEvent.setup();
+        render(<ColorPicker onChange={handleChange} />);
+
+        await user.type(screen.getByTestId("colorpicker-input"), colors[0]);
+
+        expect(handleChange).toHaveBeenCalledWith(colors[0]);
     });
 });
