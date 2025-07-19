7523d080ea810f8c0b0f80749aaa653390e17bf2
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-07-19 18:39:53 +0200

    Update default configuration

--- server/config.ts
+++ server/config.ts
@@ -1,9 +1,8 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import path from "node:path";
 import yaml from "yaml";
 
 export type AccessTokenAuthConfig = {
-    // if not provided, a token will be automatically generated
     token?: string;
     user?: string;
 };
@@ -13,9 +12,7 @@ export type AuthConfig = {
 };
 
 export type LocalStorageConfig = {
-    // path to the folder where the database will be stored
     store_path?: string;
-    // name of the database file. Defaults to "folio.db"
     store_name?: string;
 };
 
@@ -39,9 +36,7 @@ export type WebsiteConfig = {
 
 // configuration for folio server
 export type Config = {
-    // port where the server will run
     port?: number;
-    // storage and authentication configuration
     storage?: StorageConfig;
     authentication?: AuthConfig;
     security?: SecurityConfig;
@@ -49,10 +44,7 @@ export type Config = {
 };
 
 // @description read configuration file
-export const readConfig = async (configPath: string): Promise<Config|null> => {
-    if (fs.existsSync(configPath)) {
-        return yaml.parse(fs.readFileSync(configPath, "utf8"));
-    }
-    // configuration not found
-    return null;
+export const readConfig = async (configPath: string): Promise<Config> => {
+    const content = fs.readFile(path.resolve(configPath), "utf8");
+    return yaml.parse(content);
 };
