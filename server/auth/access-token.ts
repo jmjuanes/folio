7b71e16f67ab47e844d2af212731bf2bfee69279
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-07-20 10:16:49 +0200

    Improve log messages

--- server/auth/access-token.ts
+++ server/auth/access-token.ts
@@ -3,7 +3,7 @@ import { createLogger } from "../utils/logger.ts";
 import type { AccessTokenAuthConfig } from "../config.ts";
 import type { AuthContext, AuthPayload } from "../types/authentication.ts";
 
-const { log } = createLogger("folio:auth");
+const { info } = createLogger("folio:auth");
 
 const ACCESS_TOKEN_LENGTH = 24;
 const ACCESS_USER = "folio";
@@ -15,9 +15,8 @@ export const createAccessTokenAuth = async (authConfig: AccessTokenAuthConfig): 
     const accessToken = authConfig?.token || generateToken(ACCESS_TOKEN_LENGTH);
 
     // print login information in logs
-    log(`======= Authentication =======`);
-    log(`Using Access Token as authentication method.`);
-    log(`Use '${accessToken}' to login.`);
+    info(`Using Access Token as authentication method.`);
+    info(`Use '${accessToken}' to login.`);
 
     return {
         validate: async (token: string): Promise<AuthPayload|null> => {
--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,6 +1,14 @@
 export type Logger = {
     log: (message: string) => void;
+    info: (message: string) => void;
     debug: (message: string) => void;
+    error: (message: string) => void;
+};
+
+export enum LogLevels {
+    INFO = "INFO",
+    DEBUG = "DEBUG",
+    ERROR = "ERROR",
 };
 
 // get current date
@@ -9,18 +17,25 @@ const getDate = (): string => {
 };
 
 // format message
-const formatMessage = (namespace: string, message: string): string => {
-    return `[${getDate()} ${namespace}] ${message}`;
+const formatMessage = (namespace: string, level: LogLevels, message: string): string => {
+    return `[${getDate()} ${namespace}] (${level}) ${message}`;
 };
 
 // create a new logger
 export const createLogger = (namespace: string) : Logger => {
     return {
+        // @deprecated use info instead
         log: (message: string) => {
-            console.log(formatMessage(namespace, message));
+            console.log(formatMessage(namespace, LogLevels.INFO, message));
+        },
+        info: (message: string) => {
+            console.log(formatMessage(namespace, LogLevels.INFO, message));
         },
         debug: (message: string) => {
-            console.debug(formatMessage(namespace, message));
+            console.debug(formatMessage(namespace, LogLevels.DEBUG, message));
+        },
+        error: (message: string) => {
+            console.error(formatMessage(namespace, LogLevels.ERROR, message));
         },
     };
 };
