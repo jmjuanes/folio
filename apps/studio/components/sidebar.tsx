03d5dafd8f31bddda164424522e484b4865aca90
Josemi <josemijuanes@gmail.com>
2025-07-25 18:13:08 +0200

    Implemented new sidebar

--- apps/studio/components/sidebar.tsx
+++ apps/studio/components/sidebar.tsx
@@ -1,28 +1,40 @@
 import React from "react";
 import { useToggle } from "react-use";
 import classNames from "classnames";
-import { renderIcon, DrawingIcon, ChevronLeftIcon, ChevronRightIcon } from "@josemi-icons/react";
+import { renderIcon, DrawingIcon } from "@josemi-icons/react";
 import { useClient } from "../contexts/client.tsx";
 import { useRouter } from "../contexts/router.tsx";
 import { BoardLink } from "./board-link.tsx";
 import { groupByDate } from "../utils/dates.ts";
 
-// @description logo component
-const Logo = (): React.JSX.Element => (
-    <div className="sticky top-0 text-3xl leading-none select-none bg-white pt-5 px-3 pb-3">
-        <span className="text-gray-950 font-brand select-none">folio.</span>
-    </div>
-);
+type ActionButtonProps = {
+    href?: string;
+    icon?: string;
+    text?: string;
+    collapsed?: boolean;
+    onClick?: (event?: React.SyntheticEvent) => void;
+};
 
 // @description action button component (create, import, settings, etc.)
-const ActionButton = ({ href, icon, text, onClick }): React.JSX.Element => (
-    <a className="flex items-center gap-2 cursor-pointer rounded-md hover:bg-gray-100 px-2 py-1 text-gray-700 hover:text-gray-900" href={href} onClick={onClick}>
-        <div className="flex text-base">
-            {renderIcon(icon)}
-        </div>
-        <div className="text-sm font-medium">{text}</div>
-    </a>
-);
+const ActionButton = ({ href, icon, text = "", collapsed = false, onClick }: ActionButtonProps): React.JSX.Element => {
+    const actionClass = classNames({
+        "h-8 flex items-center gap-2 cursor-pointer rounded-md hover:bg-gray-200 py-1 text-gray-700 hover:text-gray-950": true,
+        "justify-center": collapsed,
+        "px-2": !collapsed,
+    });
+    return (
+        <a className={actionClass} href={href} onClick={onClick}>
+            {icon && (
+                <div className="flex text-xl">
+                    {renderIcon(icon)}
+                </div>
+            )}
+            {!collapsed && text && (
+                <div className="text-sm font-medium">{text}</div>
+            )}
+        </a>
+    );
+};
 
 const BoardsGroup = ({ title, boards, onRename, onDelete }): React.JSX.Element => {
     const [hash] = useRouter();
@@ -37,7 +49,7 @@ const BoardsGroup = ({ title, boards, onRename, onDelete }): React.JSX.Element =
                     board={item}
                     active={hash === item.id}
                     onRename={() => onRename(item.id)}
-                    onDelete={() => props.onDelete(item.id)}
+                    onDelete={() => onDelete(item.id)}
                 />
             ))}
         </div>
@@ -48,7 +60,7 @@ const BoardsGroup = ({ title, boards, onRename, onDelete }): React.JSX.Element =
 const BoardsList = ({ boards, onRename, onDelete }): React.JSX.Element => {
     const groups = React.useMemo(() => {
         return groupByDate(boards, "updated_at");
-    }, [boards]);
+    }, [boards, boards.length]);
 
     return (
         <React.Fragment>
@@ -109,67 +121,62 @@ const BoardsList = ({ boards, onRename, onDelete }): React.JSX.Element => {
     );
 };
 
-const ToggleButton = ({ style, collapsed, onClick }): React.JSX.Element => (
-    <div className="absolute left-0 top-half z-50 cursor-pointer" style={style} onClick={onClick}>
-        <div className="flex bg-gray-200 text-lg py-2 pr-1 rounded-tr-md rounded-br-md">
-            {collapsed ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-        </div>
-    </div>
-);
-
 // export the sidebar component
 export const Sidebar = (props: any): React.JSX.Element => {
     const [collapsed, toggleCollapsed] = useToggle(!!props.defaultCollapsed);
     const client = useClient();
     const sidebarClass = classNames({
-        "w-64 h-full bg-white shrink-0 flex-col justify-between border-r-1 border-gray-200": true,
-        "flex": !collapsed,
-        "hidden": collapsed,
+        "h-full bg-gray-50 shrink-0 flex flex-col justify-between border-r-1 border-gray-200": true,
+        "w-64": !collapsed,
     });
 
     return (
-        <React.Fragment>
-            <div className={sidebarClass}>
-                <div className="flex flex-col gap-2 h-full overflow-y-auto overflow-x-hidden">
-                    <Logo />
-                    <div className="flex flex-col gap-4 h-full px-3 select-none">
-                        <div className="flex flex-col gap-1 mb-2">
-                            <ActionButton href="#" icon="home" text="Home" />
-                            <ActionButton
-                                onClick={() => props.onBoardCreate()}
-                                icon="plus"
-                                text="Create a new board"
-                            />
-                            <ActionButton
-                                onClick={() => props.onBoardImport()}
-                                icon="upload"
-                                text="Import board from file"
-                            />
-                        </div>
+        <div className={sidebarClass}>
+            <div className="flex flex-col gap-2 h-full overflow-y-auto overflow-x-hidden">
+                <div className="sticky z-50 top-0 text-3xl leading-none select-none bg-gray-50 py-5 px-3 flex items-center justify-between">
+                    <div className="text-gray-950 font-brand select-none">
+                        {!collapsed && (<span>folio.</span>)}
+                    </div>
+                    <div className="">
+                        <ActionButton
+                            onClick={() => toggleCollapsed()}
+                            icon={collapsed ? "sidebar-left-open" : "sidebar-left-close"}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4 h-full px-3 select-none">
+                    <div className="flex flex-col gap-1 mb-2">
+                        <ActionButton collapsed={collapsed} href="#" icon="home" text="Home" />
+                        <ActionButton
+                            onClick={() => props.onBoardCreate()}
+                            collapsed={collapsed}
+                            icon="plus"
+                            text="Create a new board"
+                        />
+                        <ActionButton
+                            onClick={() => props.onBoardImport()}
+                            collapsed={collapsed}
+                            icon="upload"
+                            text="Import board from file"
+                        />
+                    </div>
+                    {!collapsed && (
                         <BoardsList
                             boards={props.boards}
                             onRename={props.onBoardRename}
                             onDelete={props.onBoardDelete}
                         />
-                    </div>
-                </div>
-                <div className="px-3 pt-3 pb-3 bg-white">
-                    <ActionButton
-                        icon="logout"
-                        text="Sign out"
-                        onClick={() => client.logout()}
-                    />
+                    )}
                 </div>
             </div>
-            <div className="relative h-full">
-                <ToggleButton
+            <div className="px-3 pt-3 pb-3 bg-gray-50">
+                <ActionButton
+                    onClick={() => client.logout()}
                     collapsed={collapsed}
-                    style={{
-                        transform: "translateY(-50%)",
-                    }}
-                    onClick={toggleCollapsed}
+                    icon="logout"
+                    text="Sign out"
                 />
             </div>
-        </React.Fragment>
+        </div>
     );
 };
