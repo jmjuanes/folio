bf913cf8f9700faacf6feaaf104c2afb7814c942
Josemi <josemijuanes@gmail.com>
2025-07-29 18:22:18 +0200

    Improve client and include config endpoint

--- apps/studio/contexts/client.tsx
+++ apps/studio/contexts/client.tsx
@@ -4,6 +4,7 @@ import { useApi } from "../hooks/use-api.ts";
 
 export type Client = {
     token: string;
+    config: () => Promise<any>;
     login: (credentials: any) => Promise<void>;
     logout: () => void;
     graphql: (query: string, variables?: any) => Promise<any>;
@@ -25,20 +26,21 @@ export const ClientProvider = ({ sessionKey = "", children }): React.JSX.Element
     // api to access to the REST API
     const client: Client = React.useMemo(() => ({
         token: token as string,
-        logout: () => removeToken(),
-        login: (credentials) => {
-            return api("POST", "/_login", credentials || {}).then(response => {
-                setToken(response.data.token);
+        logout: () => {
+            removeToken();
+        },
+        login: (credentials = {}) => {
+            return api("POST", "/_login", credentials).then(response => {
+                if (response?.data?.token) {
+                    setToken(response.data.token);
+                }
             });
         },
+        config: () => {
+            return api("GET", "/_config");
+        },
         graphql: (query, variables) => {
-            return api("POST", "/_graphql", {query, variables}).then(response => {
-                if (response.errors) {
-                    return Promise.reject(response);
-                }
-                // return response object
-                return response;
-            });
+            return api("POST", "/_graphql", { query, variables });
         },
     }), [token, setToken, removeToken]);
 
