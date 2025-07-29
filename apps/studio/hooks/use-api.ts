32832277ce7d2becb7027bc8a4dc3be2d7e338f1
Josemi <josemijuanes@gmail.com>
2025-07-29 18:22:04 +0200

    Improve api hook to process errors in response

--- apps/studio/hooks/use-api.ts
+++ apps/studio/hooks/use-api.ts
@@ -20,11 +20,38 @@ export const useApi = (token: string): ApiClient => {
             options.body = JSON.stringify(data);
         }
         // perform the request
-        return fetch(path, options).then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        });
+        return fetch(path, options)
+            .then(response => {
+                // if the response is not ok, we throw an error
+                if (!response.ok) {
+                    // try to parse the response as JSON to get the errors
+                    return response.json().then(responseData => {
+                        return Promise.reject(responseData);
+                    });
+                }
+                // if the response is ok, we return the response as JSON
+                return response.json();
+            })
+            .then(responseData => {
+                // if there is a field named "errors" in the response, we reject the promise
+                // this is a common pattern in GraphQL responses, but can also be used in REST
+                if (responseData.errors) {
+                    return Promise.reject(responseData);
+                }
+                // return the response object to process
+                return responseData;
+            })
+            .catch(response => {
+                // make sure that the response contains an errors array
+                if (response && Array.isArray(response.errors)) {
+                    return Promise.reject(response);
+                }
+                // if the response is not an object, we throw a generic error
+                return Promise.reject({
+                    errors: [
+                        { message: "An error occurred while processing the request." },
+                    ],
+                });
+            });
     }, [token]);
 };
