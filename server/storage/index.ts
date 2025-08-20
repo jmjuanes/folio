6133af4f12e33368d5e823a79c989a7f9f9dc2f8
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-20 23:57:46 +0200

    Allow to create memory storage

--- server/storage/index.ts
+++ server/storage/index.ts
@@ -1,5 +1,6 @@
 import { createLogger } from "../utils/logger.ts";
 import { createLocalStore } from "./local.ts";
+import { createMemoryStore } from "./memory.ts";
 import { StorageTypes } from "../config.ts";
 import type { StoreContext } from "../types/storage.ts";
 import type { Config } from "../config.ts";
@@ -11,6 +12,9 @@ export const createStore = async (config: Config): Promise<StoreContext> => {
 
     if (config?.storage === StorageTypes.LOCAL) {
         store = await createLocalStore(config);
+    }
+    else if (config?.store === StorageTypes.MEMORY) {
+        store = await createMemoryStore(config);
     }
     else {
         error("No valid storage method configured");
