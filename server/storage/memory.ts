31bfc2044f574b4d54de7893b4306a1baa341b25
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-21 12:51:51 +0200

    feat(studio): add memory storage (#181)

---
+++ server/storage/memory.ts
@@ -0,0 +1,48 @@
+import { Collections } from "../types/storage.ts";
+import type { Config } from "../config.ts";
+import type { StoreContext, Document, DocumentData } from "../types/storage.ts";
+
+// create an instance of a store
+export const createMemoryStore = async (config: Config): Promise<StoreContext> => {
+    const storage: Document[] = [];
+
+    // return api to access to the memory storage
+    return {
+        all: async (collection: Collections): Promise<Document[]> => {
+            return storage.filter(document => {
+                return document._collection === collection;
+            });
+        },
+        get: async (collection: Collections, id: string): Promise<Document> => {
+            return storage.find(document => {
+                return document._collection === collection && document._id === id;
+            });
+        },
+        add: async (collection: Collections, id: string, data: DocumentData = {}): Promise<void> => {
+            storage.push({
+                _id: id,
+                _collection: collection,
+                _created_at: Date.now(),
+                _updated_at: Date.now(),
+                ...data,
+            });
+        },
+        set: async (collection: Collections, id: string, data: DocumentData = {}): Promise<void> => {
+            const document = storage.find(document => {
+                return document._collection === collection && document._id === id;
+            });
+            if (document) {
+                Object.assign(document, data);
+                document._updated_at = Date.now();
+            }
+        },
+        delete: async (collection: Collections, id: string): Promise<void> => {
+            const index = storage.findIndex(document => {
+                return document._id === id && document._collection === collection;
+            });
+            if (index > -1) {
+                storage.splice(index, 1);
+            }
+        },
+    };
+};
--- server/types/storage.ts
+++ server/types/storage.ts
@@ -7,6 +7,7 @@ export enum Collections {
 export type DocumentData = Record<string, any>;
 
 export type Document = DocumentData & {
+    _collection?: string,
     _id: string,
     _created_at: string,
     _updated_at: string,
