974bba342dcf9cc0e99a6774d6f3a3ec61cddd0b
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-22 17:20:28 +0200

    Disable by default library components

--- folio-react/contexts/editor-components.jsx
+++ folio-react/contexts/editor-components.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import {Layout} from "../components/layout.jsx";
-import {Loading} from "../components/loading.jsx";
-import {ContextMenu} from "../components/context-menu.jsx";
-import {ExportDialog} from "../components/dialogs/export.jsx";
-import {LibraryAddDialog} from "../components/dialogs/library-add.jsx";
-import {LibraryExportDialog} from "../components/dialogs/library-export.jsx";
-import {PageEditDialog} from "../components/dialogs/page-edit.jsx";
-import {KeyboardShortcutsDialog} from "../components/dialogs/keyboard-shortcuts.jsx";
-import {LibraryMenu} from "../components/menus/library.jsx";
-import {MainMenu} from "../components/menus/main.tsx";
-import {PagesMenu} from "../components/menus/pages.jsx";
-import {SettingsMenu} from "../components/menus/settings.jsx";
-import {EditionPanel} from "../components/panels/edition.jsx";
-import {HistoryPanel} from "../components/panels/history.jsx";
-import {MinimapPanel} from "../components/panels/minimap.jsx";
-import {ToolbarPanel} from "../components/panels/toolbar.jsx";
-import {ZoomPanel} from "../components/panels/zoom.jsx";
-import {LayersPanel} from "../components/panels/layers.jsx";
-import {MenuPanel} from "../components/panels/menu.jsx";
+import { Layout } from "../components/layout.jsx";
+import { Loading } from "../components/loading.jsx";
+import { ContextMenu } from "../components/context-menu.jsx";
+import { ExportDialog } from "../components/dialogs/export.jsx";
+import { PageEditDialog } from "../components/dialogs/page-edit.jsx";
+import { KeyboardShortcutsDialog } from "../components/dialogs/keyboard-shortcuts.jsx";
+import { MainMenu } from "../components/menus/main.tsx";
+import { PagesMenu } from "../components/menus/pages.jsx";
+import { SettingsMenu } from "../components/menus/settings.jsx";
+import { EditionPanel } from "../components/panels/edition.jsx";
+import { HistoryPanel } from "../components/panels/history.jsx";
+import { MinimapPanel } from "../components/panels/minimap.jsx";
+import { ToolbarPanel } from "../components/panels/toolbar.jsx";
+import { ZoomPanel } from "../components/panels/zoom.jsx";
+import { LayersPanel } from "../components/panels/layers.jsx";
+import { MenuPanel } from "../components/panels/menu.jsx";
 
 // @description editor components context
 export const EditorComponentsContext = React.createContext(null);
@@ -31,7 +28,7 @@ export const useEditorComponents = () => {
 // @param {object} props React props
 // @param {object} props.components Editor components
 // @param {React.ReactNode} props.children React children
-export const EditorComponentsProvider = ({components = {}, children}) => {
+export const EditorComponentsProvider = ({ components = {}, children }) => {
     const editorComponents = React.useMemo(() => {
         return {
             // general components
@@ -40,12 +37,12 @@ export const EditorComponentsProvider = ({components = {}, children}) => {
             ContextMenu: ContextMenu,
             // dialogs components
             ExportDialog: ExportDialog,
-            LibraryAddDialog: LibraryAddDialog,
-            LibraryExportDialog: LibraryExportDialog,
+            LibraryAddDialog: null,
+            LibraryExportDialog: null,
             PageEditDialog: PageEditDialog,
             KeyboardShortcutsDialog: KeyboardShortcutsDialog,
             // menus
-            LibraryMenu: LibraryMenu,
+            LibraryMenu: null,
             MainMenu: MainMenu,
             PagesMenu: PagesMenu,
             SettingsMenu: SettingsMenu,
@@ -63,7 +60,7 @@ export const EditorComponentsProvider = ({components = {}, children}) => {
             // overrides
             ...components,
         };
-    }, [components]);
+    }, [ components ]);
 
     return (
         <EditorComponentsContext.Provider value={editorComponents}>
