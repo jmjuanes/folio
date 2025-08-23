3e581edacf51a764f00ba91a2533a73136ea0f2f
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-23 11:20:11 +0200

    Initialize website stuff

---
+++ website/docs/press.js
@@ -0,0 +1,44 @@
+import * as path from "node:path";
+import mikel from "mikel";
+import press from "mikel-press";
+import markdown from "mikel-markdown";
+import hljs from "highlight.js";
+import websiteConfig from "./website.config.json" with {type: "json"};
+
+press({
+    destination: "www",
+    template: mikel.create({
+        functions: {
+            highlight: params => {
+                return hljs.highlight((params?.opt?.code || "").trim(), {language: params.opt.language || "html"}).value;
+            },
+        },
+    }),
+    ...websiteConfig,
+    plugins: [
+        press.SourcePlugin({
+            folder: "content",
+        }),
+        press.PartialsPlugin({
+            folder: "partials",
+        }),
+        press.FrontmatterPlugin(),
+        press.UsePlugin(markdown({
+            classNames: {
+                link: "font-medium underline",
+                code: "bg-gray-100 rounded-md py-1 px-2 text-xs font-mono font-medium",
+                table: "w-full mb-6",
+                tableColumn: "p-3 border-b-1 border-gray-200",
+                tableHead: "font-bold",
+            },
+        })),
+        press.ContentPagePlugin(),
+        press.CopyAssetsPlugin({
+            basePath: "vendor",
+            patterns: [
+                {from: "node_modules/lowcss/low.css", to: "low.css"},
+                {from: "node_modules/highlight.js/styles/atom-one-light.css", to: "highlight.css"},
+            ],
+        }),
+    ],
+});
