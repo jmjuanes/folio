3c8179f3853cc8d2af1e6cb22b66e3dc4e6752ad
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-28 00:15:38 +0200

    Update document type

--- server/types/storage.ts
+++ server/types/storage.ts
@@ -1,23 +1,25 @@
 // available collections in the store
 export enum Collections {
     BOARD = "board",
-    LIBRARY = "library",
 };
 
-export type DocumentData = Record<string, any>;
+export type Attributes = Record<string, any>;
 
-export type Document = DocumentData & {
-    _collection?: string,
-    _id: string,
-    _created_at: string,
-    _updated_at: string,
+export type Document = {
+    collection?: string,
+    owner: string,
+    id: string,
+    created_at: string,
+    updated_at: string,
+    attributes: Attributes,
+    data?: string,
 };
 
 // objects store context
 export type StoreContext = {
-    all: (collection: Collections) => Promise<Document[]>;
+    list: (collection: Collections) => Promise<Document[]>;
     get: (collection: Collections, id: string) => Promise<Document>;
-    add: (collection: Collections, id: string, data: DocumentData) => Promise<void>;
-    set: (collection: Collections, id: string, data: DocumentData) => Promise<void>;
+    add: (collection: Collections, id: string, attributes?: Attributes, data?: string) => Promise<void>;
+    update: (collection: Collections, id: string, attributes?: Attributes, data?: DocumentData) => Promise<void>;
     delete: (collection: Collections, id: string) => Promise<void>;
 };
