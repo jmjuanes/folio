dd18066c0e9d1685735723b9910d32a344fef3c7
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-28 16:55:17 +0200

    Fix wrog argument type in sotrage content

--- server/types/storage.ts
+++ server/types/storage.ts
@@ -5,6 +5,6 @@ export type StoreContext = {
     list: (collection: Collections) => Promise<Document[]>;
     get: (collection: Collections, id: string) => Promise<Document>;
     add: (collection: Collections, id: string, attributes?: Attributes, data?: string) => Promise<void>;
-    update: (collection: Collections, id: string, attributes?: Attributes, data?: DocumentData) => Promise<void>;
+    update: (collection: Collections, id: string, attributes?: Attributes, data?: string) => Promise<void>;
     delete: (collection: Collections, id: string) => Promise<void>;
 };
