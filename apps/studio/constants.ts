4c85ff584f44f5ad1bcbb10ceec0b9067f7a0ede
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2025-08-31 11:59:24 +0200

    Remove unused contexts and hooks

--- apps/studio/constants.ts
+++ apps/studio/constants.ts
@@ -1,18 +1,3 @@
-// list of available collections
 export enum COLLECTIONS {
     BOARD = "board",
-};
-
-export enum ACTIONS {
-    GET_BOARD = "getBoard",
-    GET_RECENT_BOARDS = "getRecentBoards",
-    CREATE_BOARD = "createBoard",
-    IMPORT_BOARD = "importBoard",
-    DELETE_BOARD = "deleteBoard",
-    UPDATE_BOARD = "updateBoard",
-    SHOW_RENAME_BOARD_DIALOG = "showRenameBoardDialog",
-};
-
-export enum EVENT_NAMES {
-    BOARD_ACTION = "board:action",
 };
--- apps/studio/index.tsx
+++ apps/studio/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { ConfirmProvider } from "folio-react/contexts/confirm.jsx";
 import { DialogsProvider } from "folio-react/contexts/dialogs.jsx";
 import { ConfigurationProvider } from "./contexts/configuration.tsx";
-import { ClientProvider } from "./contexts/client.tsx";
 import { AuthenticationProvider } from "./contexts/authentication.tsx";
 import { RouterProvider } from "./contexts/router.tsx";
 import { ToasterProvider } from "./contexts/toaster.tsx";
