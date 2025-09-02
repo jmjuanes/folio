87cb53ca08efba7518268c81d2e3583357b14bd7
Josemi <josemijuanes@gmail.com>
2025-09-02 18:05:25 +0200

    Adapt storage to new types

--- server/storage/index.ts
+++ server/storage/index.ts
@@ -2,12 +2,12 @@ import { createLogger } from "../utils/logger.ts";
 import { createLocalStore } from "./local.ts";
 import { createMemoryStore } from "./memory.ts";
 import { StorageTypes } from "../config.ts";
-import type { StoreContext } from "../types/storage.ts";
+import type { Storage } from "../types/storage.ts";
 import type { Config } from "../config.ts";
 
-const { debug, error } = createLogger("folio:storage");
+const log = createLogger("folio:storage");
 
-export const createStore = async (config: Config): Promise<StoreContext> => {
+export const createStore = async (config: Config): Promise<Storage> => {
     let store = null;
 
     if (config?.storage === StorageTypes.LOCAL) {
@@ -17,7 +17,7 @@ export const createStore = async (config: Config): Promise<StoreContext> => {
         store = await createMemoryStore(config);
     }
     else {
-        error("No valid storage method configured");
+        log.error("No valid storage method configured");
         process.exit(1);
     }
 
