157e49241e59441010007b0679502f56b5425864
Josemi <josemijuanes@gmail.com>
2025-09-02 18:05:49 +0200

    Remove collection param from documents route

--- server/routes/documents.ts
+++ server/routes/documents.ts
@@ -4,6 +4,7 @@ import { createLogger } from "../utils/logger.ts";
 import { authentication } from "../middlewares/authentication.ts";
 import { API_ERROR_MESSAGES, HTTP_CODES } from "../constants.ts";
 import type { ExtendedContext } from "../types/custom.ts";
+import type { DocumentFilter } from "../types/document.ts";
 
 const log = createLogger("folio:route:documents");
 export const documentsRouter = new Router();
@@ -11,49 +12,45 @@ export const documentsRouter = new Router();
 // accessing documents routes requires authentication
 documentsRouter.use(authentication);
 
-// GET /
+// GET / --> return the list of documents of the authenticated user
 documentsRouter.get("/", async (ctx: ExtendedContext) => {
-    ctx.error(HTTP_CODES.METHOD_NOT_ALLOWED, API_ERROR_MESSAGES.METHOD_NOT_ALLOWED);
-});
-
-// GET /:collection --> return list of documents of the given collection
-documentsRouter.get("/:collection", async (ctx: ExtendedContext) => {
-    const collection = ctx.params.collection;
+    const filters = ctx.request.query || {} as DocumentFilter;
+    const username = ctx.state.username as string;
     try {
-        const results = await ctx.state.store.list(collection);
+        const results = await ctx.state.store.all(username, filters);
         return ctx.ok({
             data: results,
         });
     } catch (error) {
-        log.error(`error getting documents of the collection '${collection}': ${error.message}`);
+        log.error(`error getting documents: ${error.message}`);
         ctx.error(HTTP_CODES.INTERNAL_SERVER_ERROR, API_ERROR_MESSAGES.INTERNAL_SERVER_ERROR);
     }
 });
 
-// POST /:collection --> add a new document to this collection
-documentsRouter.post("/:collection", async (ctx: ExtendedContext) => {
-    const collection = ctx.params.collection;
-    const { attributes, data } = ctx.request.body;
+// POST / --> add a new document
+documentsRouter.post("/", async (ctx: ExtendedContext) => {
+    const username = ctx.state.username as string;
+    const { collection, name, thumbnail, data } = ctx.request.body;
     try {
         const newDocumentId = uid(20); // generate a unique ID for the object
-        await ctx.state.store.add(collection, newDocumentId, attributes, data);
+        await ctx.state.store.add(username, newDocumentId, { collection, name, thumbnail, data });
         return ctx.ok({
             data: {
                 id: newDocumentId,
-                attributes: attributes,
             },
         });
     } catch (error) {
-        log.error(`error adding a new document in collection '${collection}': ${error.message}`);
+        log.error(`error adding a new document: ${error.message}`);
         ctx.error(HTTP_CODES.INTERNAL_SERVER_ERROR, API_ERROR_MESSAGES.INTERNAL_SERVER_ERROR);
     }
 });
 
-// GET /:collection/:id --> get the document
-documentsRouter.get("/:collection/:id", async (ctx: ExtendedContext) => {
-    const { collection, id } = ctx.params;
+// GET /:id --> get the document with the given id
+documentsRouter.get("/:id", async (ctx: ExtendedContext) => {
+    const { id } = ctx.params;
+    const username = ctx.state.username as string;
     try {
-        const result = await ctx.state.store.get(collection, id);
+        const result = await ctx.state.store.get(username, id);
         return ctx.ok({
             data: result,
         });
@@ -63,12 +60,13 @@ documentsRouter.get("/:collection/:id", async (ctx: ExtendedContext) => {
     }
 });
 
-// PATCH /:collection/:id --> update the data/attributes of the document
-documentsRouter.patch("/:collection/:id", async (ctx: ExtendedContext) => {
-    const { collection, id } = ctx.params;
-    const { data, attributes } = ctx.request.body;
+// PATCH /:id --> update the document
+documentsRouter.patch("/:id", async (ctx: ExtendedContext) => {
+    const username = ctx.state.username as string;
+    const { id } = ctx.params;
+    const { name, thumbnail, data } = ctx.request.body;
     try {
-        await ctx.state.store.update(collection, id, attributes, data);
+        await ctx.state.store.update(username, id, { name, thumbnail, data });
         return ctx.ok({
             data: {
                 id: id,
@@ -80,11 +78,12 @@ documentsRouter.patch("/:collection/:id", async (ctx: ExtendedContext) => {
     }
 });
 
-// DELETE /:collection/:id --> delete the document
-documentsRouter.delete("/:collection/:id", async (ctx: ExtendedContext) => {
-    const { collection, id } = ctx.params;
+// DELETE /:id --> delete the document
+documentsRouter.delete("/:id", async (ctx: ExtendedContext) => {
+    const username = ctx.state.username as string;
+    const { id } = ctx.params;
     try {
-        await ctx.state.store.delete(collection, id);
+        await ctx.state.store.delete(username, id);
         return ctx.ok({
             data: {
                 id: id,
