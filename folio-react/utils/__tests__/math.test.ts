34c41fef05eb4c967196d58c589eff827545d58f
Josemi <josemijuanes@gmail.com>
2025-09-24 20:50:09 +0200

    Move math tests

---
+++ folio-react/utils/__tests__/math.test.ts
@@ -0,0 +1,70 @@
+import {
+    getPointsDistance,
+    getBoundingRectangle,
+    simplifyPath,
+    getPointDistanceToLine,
+    Point,
+} from "../math.ts";
+
+
+describe("getPointsDistance", () => {
+    it("should calculate the distance between two points", () => {
+        expect(getPointsDistance([0,0], [10,0])).toEqual(10);
+    });
+
+    it("should calculate the distance between the given points", () => {
+        expect(getPointsDistance([0,0], [10,0], [10,5])).toEqual(15);
+    });
+});
+
+describe("getBoundingRectangle", () => {
+    it("should generate the bounds", () => {
+        const originalBounds = [ [ 10, 10 ], [ 15, 15 ], [ 12, 5 ], [5, 20 ]] as Point[];
+        const newBounds = getBoundingRectangle(originalBounds);
+
+        expect(newBounds[0][0]).toEqual(5);
+        expect(newBounds[1][0]).toEqual(15);
+        expect(newBounds[0][1]).toEqual(5);
+        expect(newBounds[1][1]).toEqual(20);
+    });
+});
+
+describe("simplifyPath", () => {
+    const points: Point[] = [
+        [ 0, 0 ], [ 1, 0.1 ], [ 2, -0.1 ], [ 3, 5 ], [ 4, 6 ], [ 5, 7 ], [ 6, 8.1 ], [ 7, 9 ], [ 8, 9 ], [ 9, 9 ],
+    ];
+
+    it("should return the same start and end", () => {
+        const newPoints = simplifyPath(points, 1);
+
+        expect(newPoints[0][0]).toBe(points[0][0]);
+        expect(newPoints[0][1]).toBe(points[0][1]);
+        expect(newPoints[newPoints.length - 1][0]).toBe(points[points.length - 1][0]);
+        expect(newPoints[newPoints.length - 1][1]).toBe(points[points.length - 1][1]);
+    });
+
+    it("should return less points", () => {
+        const newPoints = simplifyPath(points, 1);
+
+        expect(newPoints.length).toBeLessThan(points.length);
+    });
+
+    it("should return points in the original points list", () => {
+        const newPoints = simplifyPath(points, 1);
+
+        expect(newPoints[1][0]).toBe(points[2][0]);
+        expect(newPoints[1][1]).toBe(points[2][1]);
+        expect(newPoints[2][0]).toBe(points[3][0]);
+        expect(newPoints[2][1]).toBe(points[3][1]);
+        expect(newPoints[3][0]).toBe(points[7][0]);
+        expect(newPoints[3][1]).toBe(points[7][1]);
+    });
+});
+
+describe("getPointDistanceToLine", () => {
+    it("should return the distance of a point to the given line", () => {
+        const distance = getPointDistanceToLine([1, 4], [[2, 3], [5, 6]]);
+
+        expect(distance).toEqual(expect.closeTo(1.41421, 5));
+    });
+});
