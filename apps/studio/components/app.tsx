73074df5acbd706e838d25a6d9f2eb713948a788
Josemi <josemijuanes@gmail.com>
2025-10-03 18:48:00 +0200

    Improve app layout

--- apps/studio/components/app.tsx
+++ apps/studio/components/app.tsx
@@ -9,22 +9,24 @@ export const App = (): React.JSX.Element => {
     const [ hash ] = useRouter();
     
     return (
-        <div className="fixed top-0 left-0 h-full w-full bg-white text-gray-800 flex">
+        <div className="fixed top-0 left-0 h-full w-full bg-gray-100 text-gray-800 flex py-2 pr-2 pl-0">
             <Sidebar />
-            <Switch>
-                <Route test={/^#(|home)$/} render={() => (
-                    <HomeRoute />
-                )} />
-                <Route test={/^#[\w-]+$/} render={() => (
-                    <EditorRoute
-                        key={hash}
-                        id={hash.replace(/^#/, "")}
-                    />
-                )} />
-                <Route test="*" render={() => (
-                    <NotFound />
-                )} />
-            </Switch>
+            <div className="w-full h-full overflow-hidden rounded-3xl bg-white">
+                <Switch>
+                    <Route test={/^#(|home)$/} render={() => (
+                        <HomeRoute />
+                    )} />
+                    <Route test={/^#[\w-]+$/} render={() => (
+                        <EditorRoute
+                            key={hash}
+                            id={hash.replace(/^#/, "")}
+                        />
+                    )} />
+                    <Route test="*" render={() => (
+                        <NotFound />
+                    )} />
+                </Switch>
+            </div>
         </div>
     );
 };
