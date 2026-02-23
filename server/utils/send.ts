ed94868cbe9254bebf71cf96b55845e20b1dee4c
Josemi <josemijuanes@gmail.com>
2026-02-23 19:40:02 +0100

    Minor fixes in utilities to send data

--- server/utils/send.ts
+++ server/utils/send.ts
@@ -1,8 +1,8 @@
-import Koa from "koa";
+// import type { Context } from "koa";
 import { HTTP_CODES } from "../constants.ts";
 
 // send the provided response data
-export const sendData = (context: Koa.Context, data: any = {}) => {
+export const sendData = (context: any, data: any = {}) => {
     context.status = HTTP_CODES.OK;
     context.body = {
         data: data,
@@ -10,7 +10,7 @@ export const sendData = (context: Koa.Context, data: any = {}) => {
 };
 
 // send the provided error message
-export const sendError = (context: Koa.Context, errorCode: number, errorMessage: string) => {
+export const sendError = (context: any, errorCode: number, errorMessage: string) => {
     context.status = errorCode;
     context.body = {
         errors: [
