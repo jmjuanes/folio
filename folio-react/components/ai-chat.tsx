66d06b7f305d9d7dbbc73da5a0269e00574f33f4
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2026-02-27 20:41:04 +0100

    Add new chat input component

--- folio-react/components/ai-chat.tsx
+++ folio-react/components/ai-chat.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import classNames from "classnames";
+import { ArrowUpIcon } from "@josemi-icons/react";
 import { Panel } from "./ui/panel.tsx";
 import { useAi } from "../contexts/ai.tsx";
 import { useEditor } from "../contexts/editor.jsx";
-import { ELEMENTS } from "../constants.js";
 
-export const AiChat = () => {
+type AiChatInputProps = {
+    placeholder?: string;
+    disabled?: boolean;
+    inputRows?: number;
+    onSubmit: (inputText: string) => void;
+};
+
+const AiChatInput = (props: AiChatInputProps): React.JSX.Element => {
+    const inputRef = React.useRef<HTMLTextAreaElement>(null);
+    const handleSubmit = React.useCallback(() => {
+        props.onSubmit(inputRef.current?.value || "");
+    }, [props.onSubmit]);
+
+    return (
+        <div className="bg-gray-100 rounded-lg border-0 p-1">
+            <textarea
+                ref={inputRef}
+                disabled={!!props.disabled}
+                placeholder={props.placeholder}
+                rows={props?.inputRows ?? 4}
+                className="text-sm bg-transparent outline-none border-0 w-full min-h-24"
+            />
+            <div className="flex items-center justify-end">
+                <button className="border-0 bg-gray-950 hover:bg-gray-900 cursor-pointer text-white flex rounded-full p-1" onClick={handleSubmit}>
+                    <ArrowUpIcon />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export const AiChat = (): React.JSX.Element => {
     const [activeChatId, setActiveChatId] = React.useState<string>("");
     const scrollRef = React.useRef<HTMLDivElement>(null);
     const ai = useAi();
@@ -73,10 +104,11 @@ export const AiChat = () => {
                         <span>Messages</span>
                     </div>
                     <div className="w-full shrink-0">
-                        <textarea
-                            placeholder="Type your message..."
-                            className="w-full border-0 bg-gray-100 rounded-lg text-sm outline-none min-h-24"
-                            rows={4}
+                        <AiChatInput
+                            placeholder="Ask anything..."
+                            onSubmit={(prompt: string) => {
+                                console.log(prompt);
+                            }}
                         />
                     </div>
                 </Panel.Body>
