8347ea4dfc05adc5165623ea8fe52ec0d2e0ed6c
Josemi <josemijuanes@gmail.com>
2026-04-04 21:39:12 +0000

    Fix eraser tool

--- folio-react/tools/eraser.ts
+++ folio-react/tools/eraser.ts
@@ -1,11 +1,25 @@
-import { TOOLS, ELEMENTS } from "../constants.js";
+import {
+    TOOLS,
+    ELEMENTS,
+    ERASER_COLOR,
+    ERASER_OPACITY,
+    ERASER_SIZE,
+    ERASER_FADE_DELAY,
+} from "../constants.js";
 import { getElementNormalizedPosition } from "../lib/elements.js";
 import { ToolState } from "../lib/tool.ts";
 import type { EditorPointEvent } from "../lib/events.ts";
 
+const getPointerPosition = (event: EditorPointEvent): [number, number] => {
+    return [
+        event.originalX + (event.dx || 0),
+        event.originalY + (event.dy || 0),
+    ];
+};
+
 export class EraserTool extends ToolState {
     id = TOOLS.ERASER;
-    pointerId: string | null = null;
+    private pointerId: string | null = null;
 
     onEnter() {
         // when entering in the eraser tool, reset the erased key in all elements
@@ -13,22 +27,23 @@ export class EraserTool extends ToolState {
         this.editor.getElements().forEach((element: any) => {
             element.erased = false;
         });
+        this.pointerId = null;
     }
 
     onPointerDown(event: EditorPointEvent) {
+        const [x, y] = getPointerPosition(event);
         this.pointerId = this.editor.pointer.start({
-            color: "#cbd5e1", // Light grey for eraser trail
-            size: 20,         // Larger size for eraser
-            opacity: 0.5,
+            color: ERASER_COLOR,
+            size: ERASER_SIZE,
+            opacity: ERASER_OPACITY,
+            fadeDelay: ERASER_FADE_DELAY,
         });
-        this.addPoint(event);
+        this.editor.pointer.addPoint(this.pointerId, x, y);
     }
 
     onPointerMove(event: EditorPointEvent) {
-        this.addPoint(event);
-        
-        const x = event.originalX + (event.dx || 0);
-        const y = event.originalY + (event.dy || 0);
+        const [x, y] = getPointerPosition(event);
+        this.editor.pointer.addPoint(this.pointerId, x, y);
         this.editor.getElements().forEach((element: any) => {
             if (!element.erased) {
                 const b = element.type === ELEMENTS.ARROW ? getElementNormalizedPosition(element) : element;
@@ -44,21 +59,13 @@ export class EraserTool extends ToolState {
             this.editor.pointer.finish(this.pointerId);
             this.pointerId = null;
         }
-
+        // get the elements that has to be removed from the board
         const erasedElements = this.editor.getElements().filter((element: any) => {
             return element.erased;
         });
         if (erasedElements.length > 0) {
             this.editor.removeElements(erasedElements);
             this.editor.dispatchChange();
-        }
-    }
-
-    addPoint(event: EditorPointEvent) {
-        if (this.pointerId) {
-            const x = event.originalX + (event.dx || 0);
-            const y = event.originalY + (event.dy || 0);
-            this.editor.pointer.addPoint(this.pointerId, x, y);
         }
     }
 };
