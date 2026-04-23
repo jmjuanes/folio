8e4f3428c731fe447ab9ceb4b91715134ed6941f
Josemi <josemijuanes@gmail.com>
2026-04-23 18:39:43 +0000

    Fix webpack configuration to support react runtime automatic

--- apps/studio/webpack.config.js
+++ apps/studio/webpack.config.js
@@ -62,6 +62,12 @@ export default {
                     presets: [
                         "@babel/preset-env", 
                         "@babel/preset-react",
+                        [
+                            "@babel/preset-react",
+                            {
+                                runtime: "automatic",
+                            },
+                        ],
                         "@babel/preset-typescript",
                     ],
                     plugins: [
