19354952439952eed0f51d6704071730cae8e463
Josemi <josemijuanes@gmail.com>
2026-05-24 09:19:50 +0000

    Add quotas in ai components

--- folio-react/components/ui/ai.tsx
+++ folio-react/components/ui/ai.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import classNames from "classnames";
 import { renderIcon } from "@josemi-icons/react";
+import type { JSX, ReactNode } from "react";
 
 export type AiPromptProps = {
     className?: string;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiPromptInputProps = {
@@ -35,23 +36,23 @@ export type AiPromptButtonProps = {
 export type AiConversationProps = {
     className?: string;
     scrollToEnd?: boolean;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiMessageProps = {
     className?: string;
     role: string;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiMessageContentProps = {
     className?: string;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiMessageActionsProps = {
     className?: string;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiMessageActionProps = {
@@ -64,12 +65,12 @@ export type AiMessageActionProps = {
 
 export type AiArtifactProps = {
     className?: string;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiArtifactHeaderProps = {
     className?: string;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiArtifactTitleProps = {
@@ -80,7 +81,7 @@ export type AiArtifactTitleProps = {
 
 export type AiArtifactActionsProps = {
     className?: string;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 export type AiArtifactActionProps = {
@@ -96,31 +97,37 @@ export type AiContextProps = {
     usedRequests: number;
 };
 
+export type AiQuotasProps = {
+    requestsLimit: number;
+    requestsUsed: number;
+};
+
 export type AiComponents = {
-    Prompt: (props: AiPromptProps) => React.JSX.Element;
-    PromptInput: (props: AiPromptInputProps) => React.JSX.Element;
-    PromptSubmit: (props: AiPromptSubmitProps) => React.JSX.Element;
-    PromptButton: (props: AiPromptButtonProps) => React.JSX.Element;
-    Conversation: (props: AiConversationProps) => React.JSX.Element;
-    Message: (props: AiMessageProps) => React.JSX.Element;
-    MessageContent: (props: AiMessageContentProps) => React.JSX.Element;
-    MessageActions: (props: AiMessageActionsProps) => React.JSX.Element;
-    MessageAction: (props: AiMessageActionProps) => React.JSX.Element;
-    Artifact: (props: AiArtifactProps) => React.JSX.Element;
-    ArtifactHeader: (props: AiArtifactHeaderProps) => React.JSX.Element;
-    ArtifactTitle: (props: AiArtifactTitleProps) => React.JSX.Element;
-    ArtifactActions: (props: AiArtifactActionsProps) => React.JSX.Element;
-    ArtifactAction: (props: AiArtifactActionProps) => React.JSX.Element;
-    Context: (props: AiContextProps) => React.JSX.Element;
+    Prompt: (props: AiPromptProps) => JSX.Element;
+    PromptInput: (props: AiPromptInputProps) => JSX.Element;
+    PromptSubmit: (props: AiPromptSubmitProps) => JSX.Element;
+    PromptButton: (props: AiPromptButtonProps) => JSX.Element;
+    Conversation: (props: AiConversationProps) => JSX.Element;
+    Message: (props: AiMessageProps) => JSX.Element;
+    MessageContent: (props: AiMessageContentProps) => JSX.Element;
+    MessageActions: (props: AiMessageActionsProps) => JSX.Element;
+    MessageAction: (props: AiMessageActionProps) => JSX.Element;
+    Artifact: (props: AiArtifactProps) => JSX.Element;
+    ArtifactHeader: (props: AiArtifactHeaderProps) => JSX.Element;
+    ArtifactTitle: (props: AiArtifactTitleProps) => JSX.Element;
+    ArtifactActions: (props: AiArtifactActionsProps) => JSX.Element;
+    ArtifactAction: (props: AiArtifactActionProps) => JSX.Element;
+    Context: (props: AiContextProps) => JSX.Element;
+    Quotas: (props: AiQuotasProps) => JSX.Element | null;
 };
 
 export const Ai: AiComponents = {
-    Prompt: (props: AiPromptProps): React.JSX.Element => (
+    Prompt: (props: AiPromptProps): JSX.Element => (
         <div className={classNames("bg-white border-1 border-gray-200 rounded-xl p-3", props.className)}>
             {props.children}
         </div>
     ),
-    PromptInput: (props: AiPromptInputProps): React.JSX.Element => (
+    PromptInput: (props: AiPromptInputProps): JSX.Element => (
         <textarea
             disabled={!!props.disabled}
             placeholder={props.placeholder}
@@ -131,7 +138,7 @@ export const Ai: AiComponents = {
             style={{ resize: "none" }}
         />
     ),
-    PromptButton: (props: AiPromptButtonProps): React.JSX.Element => {
+    PromptButton: (props: AiPromptButtonProps): JSX.Element => {
         const buttonClassName = classNames({
             "border-0 flex items-center gap-1 rounded-lg p-2 bg-transparent": true,
             "cursor-pointer hover:bg-gray-100": !props.disabled,
@@ -148,7 +155,7 @@ export const Ai: AiComponents = {
             </button>
         );
     },
-    PromptSubmit: (props: AiPromptSubmitProps): React.JSX.Element => {
+    PromptSubmit: (props: AiPromptSubmitProps): JSX.Element => {
         const buttonClassName = classNames({
             "border-0 flex items-center rounded-full p-2": true,
             "bg-gray-950 hover:bg-gray-900 text-white": true,
@@ -166,9 +173,9 @@ export const Ai: AiComponents = {
             </button>
         );
     },
-    Conversation: (props: AiConversationProps): React.JSX.Element => {
-        const scrollRef = React.useRef<HTMLDivElement>(null);
-        React.useEffect(() => {
+    Conversation: (props: AiConversationProps): JSX.Element => {
+        const scrollRef = useRef<HTMLDivElement>(null);
+        useEffect(() => {
             if (props.scrollToEnd && scrollRef.current) {
                 // scrollRef.current.scrollIntoView({ behavior: "smooth" });
                 scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
@@ -180,7 +187,7 @@ export const Ai: AiComponents = {
             </div>
         );
     },
-    Message: (props: AiMessageProps): React.JSX.Element => {
+    Message: (props: AiMessageProps): JSX.Element => {
         const messageClassName = classNames({
             "flex flex-col w-full": true,
             "items-end": props.role === "user",
@@ -192,17 +199,17 @@ export const Ai: AiComponents = {
             </div>
         );
     },
-    MessageContent: (props: AiMessageContentProps): React.JSX.Element => (
+    MessageContent: (props: AiMessageContentProps): JSX.Element => (
         <div className={classNames("flex flex-col gap-1", props.className)}>
             {props.children}
         </div>
     ),
-    MessageActions: (props: AiMessageActionsProps): React.JSX.Element => (
+    MessageActions: (props: AiMessageActionsProps): JSX.Element => (
         <div className={classNames("flex items-center gap-1", props.className)}>
             {props.children}
         </div>
     ),
-    MessageAction: (props: AiMessageActionProps): React.JSX.Element => {
+    MessageAction: (props: AiMessageActionProps): JSX.Element => {
         const buttonClassName = classNames({
             "flex flex-row items-center gap-1 p-1 rounded-lg bg-transparent border-0": true,
             "hover:bg-gray-100 cursor-pointer": !props.disabled,
@@ -219,27 +226,27 @@ export const Ai: AiComponents = {
             </button>
         );
     },
-    Artifact: (props: AiArtifactProps): React.JSX.Element => (
+    Artifact: (props: AiArtifactProps): JSX.Element => (
         <div className={classNames("flex flex-col overflow-hidden rounded-xl border-1 border-gray-200", props.className)}>
             {props.children}
         </div>
     ),
-    ArtifactHeader: (props: AiArtifactHeaderProps): React.JSX.Element => (
+    ArtifactHeader: (props: AiArtifactHeaderProps): JSX.Element => (
         <div className={classNames("flex items-center justify-between p-2 bg-gray-50 border-b-1 border-gray-200", props.className)}>
             {props.children}
         </div>
     ),
-    ArtifactTitle: (props: AiArtifactTitleProps): React.JSX.Element => (
+    ArtifactTitle: (props: AiArtifactTitleProps): JSX.Element => (
         <div className={classNames("text-sm font-bold", props.className)}>
             {props.text || props.title}
         </div>
     ),
-    ArtifactActions: (props: AiArtifactActionsProps): React.JSX.Element => (
+    ArtifactActions: (props: AiArtifactActionsProps): JSX.Element => (
         <div className={classNames("flex items-center gap-1", props.className)}>
             {props.children}
         </div>
     ),
-    ArtifactAction: (props: AiArtifactActionProps): React.JSX.Element => {
+    ArtifactAction: (props: AiArtifactActionProps): JSX.Element => {
         const buttonClassName = classNames({
             "flex flex-row gap-1 p-2 rounded-lg": true,
             "hover:bg-gray-100 cursor-pointer": !props.disabled,
@@ -256,7 +263,7 @@ export const Ai: AiComponents = {
             </button>
         );
     },
-    Context: (props: AiContextProps): React.JSX.Element => {
+    Context: (props: AiContextProps): JSX.Element => {
         const completed = props.usedRequests / props.maxRequests;
         return (
             <div className="flex flex-col items-center gap-1">
@@ -269,5 +276,22 @@ export const Ai: AiComponents = {
                 </div>
             </div>
         );
+    },
+    Quotas: (props: AiQuotasProps): JSX.Element | null => {
+        if (props.requestsUsed > 0) {
+            const completed = props.requestsUsed / props.requestsLimit;
+            return (
+                <div className="flex flex-col items-center gap-1">
+                    <div className="flex h-2 rounded-full w-full bg-gray-100 overflow-hidden">
+                        <div className="bg-gray-950 h-2" style={{ width: `${100 * completed}%` }} />
+                    </div>
+                    <div className="text-xs opacity-80 text-center">
+                        <span className="font-bold">{props.requestsLimit - props.requestsUsed}</span>
+                        <span> requests left today.</span>
+                    </div>
+                </div>
+            );
+        }
+        return null;
     },
 };
