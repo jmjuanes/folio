c223c71b1e35e694afdd65dc3f759717007b6fe5
Josemi <josemijuanes@gmail.com>
2026-05-29 22:26:30 +0000

    Initialize script for building landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,6 +3,7 @@ import { basename, extname, join } from "node:path";
 import { build, createInput } from "mikel-cli";
 import mikel from "mikel";
 import frontmatterPlugin from "mikel-frontmatter";
+import markdownPlugin from "mikel-markdown";
 
 import site from "../resources/sites/landing.json" with { type: "json" };
 
@@ -14,6 +15,30 @@ const getPartials = (folder = "") => {
         }
         return partials;
     }, {});
+};
+
+const getChangelog = (changelogPath) => {
+    const changelogContent = readFileSync(changelogPath, "utf8");
+    const blocks = changelogContent.split("\n").filter(Boolean).slice(1).reduce((blocks, line) => {
+        if (line.startsWith("## ")) {
+            blocks.push([]);
+        }
+        if (line.trim().length > 0) {
+            blocks[blocks.length - 1].push(line);
+        }
+        return blocks;
+    }, []);
+    // parse blocks
+    return blocks.map(lines => {
+        const [versionAndDate, title] = lines[0].replace("## ", "").split(" - ");
+        return {
+            title: title.trim(),
+            version: versionAndDate.split("(")[0].trim(),
+            date: versionAndDate.split("(")[1].replace(")", "").trim(),
+            description: lines[1].trim(),
+            content: blocks.slice(2).join("\n"),
+        };
+    });
 };
 
 build({
@@ -31,6 +56,8 @@ build({
     },
     plugins: [
         mikel.SetStatePlugin("site", site),
+        mikel.SetStatePlugin("changelog", getChangelog(join(process.cwd(), "CHANGELOG.md"))),
         frontmatterPlugin(),
+        markdownPlugin({}),
     ],
 });
