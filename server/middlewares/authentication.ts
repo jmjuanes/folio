65cea847cee81620112f54c20421c4bc8a58b021
Josemi <josemijuanes@gmail.com>
2026-06-03 16:50:14 +0000

    Fix typescript issues in authentication middleware

--- server/middlewares/authentication.ts
+++ server/middlewares/authentication.ts
@@ -1,11 +1,13 @@
 import { verifyJwtToken } from "../token.ts";
 import { API_ERROR_MESSAGES, HTTP_CODES } from "../constants.ts";
+import { sendErrorResponse } from "../utils/response.ts";
+
+import type { Context } from "koa";
 import type { Config } from "../config.ts";
-import type { ExtendedContext } from "../types/custom.ts";
 import type { TokenPayload } from "../types/authentication.ts";
 
 // authentication middleware
-export const authentication = async (ctx: ExtendedContext, next: () => Promise<any>) => {
+export const authentication = async (ctx: Context, next: () => Promise<any>) => {
     const config = ctx.state?.config as Config;
     const authHeader = ctx.headers?.authorization as string | undefined;
     const token = authHeader && authHeader.split(" ")[1];
@@ -13,17 +15,17 @@ export const authentication = async (ctx: ExtendedContext, next: () => Promise<a
     // validate that token has been provided in the authorization header
     // if not, return 401 Unauthorized
     if (!token) {
-        return ctx.error(HTTP_CODES.UNAUTHORIZED, API_ERROR_MESSAGES.AUTHENTICATION_REQUIRED);
+        return sendErrorResponse(ctx, HTTP_CODES.UNAUTHORIZED, API_ERROR_MESSAGES.AUTHENTICATION_REQUIRED);
     }
 
     // verify the token and extract user information
     // if the JWT is not valid, this method will return null
-    const tokenPayload: TokenPayload|null = verifyJwtToken(token, {
+    const tokenPayload: TokenPayload | null = verifyJwtToken(token, {
         secret: config?.jwt_token_secret,
     });
 
     if (!tokenPayload?.username) {
-        return ctx.error(HTTP_CODES.FORBIDDEN, API_ERROR_MESSAGES.INVALID_TOKEN);
+        return sendErrorResponse(ctx, HTTP_CODES.FORBIDDEN, API_ERROR_MESSAGES.INVALID_TOKEN);
     }
 
     // attach user information to the context state for use in subsequent middleware or routes
