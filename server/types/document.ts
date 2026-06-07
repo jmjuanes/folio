588468ed7bf6368d895cf42597dc6e832bbb635a
Josemi <josemijuanes@gmail.com>
2026-06-07 22:14:04 +0000

    New document types

--- server/types/document.ts
+++ server/types/document.ts
@@ -1,30 +1,22 @@
-// @description list of available collections
-export enum Collection {
-    BOARD = "board",
-};
-
-// @description main document 
-export type Document = {
-    collection?: Collection,
-    owner?: string,
+// base document fields
+export type DocumentBase = {
     id: string,
     created_at: string,
     updated_at: string,
-    name?: string,
-    attributes?: string,
-    data?: string,
 };
 
-// declare document payload
-// this is the accepted payload when creating or updating a document
-export type DocumentPayload = {
-    collection?: Collection; // collection is only required when creating a new document
-    name?: string;
-    attributes?: string;
-    data?: string;
+// @description main document 
+export type Document = DocumentBase & {
+    attributes: any,
+    data: any,
 };
 
-// accepted fields to filter documents
-export type DocumentFilter = {
-    collection?: Collection;
+// document with only attributes
+export type DocumentWithAttributes = DocumentBase & {
+    attributes: any;
+};
+
+// document with only data
+export type DocumentWithData = DocumentBase & {
+    data: any;
 };
