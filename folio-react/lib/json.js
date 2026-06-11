693605fed055f1399ef3d952e2f7d20f99734001
Josemi <josemijuanes@gmail.com>
2026-06-11 16:03:15 +0000

    Include elements field in exported json

--- folio-react/lib/json.js
+++ folio-react/lib/json.js
@@ -5,14 +5,14 @@ import {BACKGROUND_COLORS} from "../utils/colors.js";
 import {migrate} from "./migrate.js";
 
 export const saveAsJson = data => {
-    const pages = data?.pages || [];
     const exportData = {
         type: MIME_TYPES.FOLIO,
         version: VERSION,
         title: data?.title || "Untitled",
         createdAt: data?.createdAt,
         updatedAt: data?.updatedAt,
-        pages: pages,
+        pages: data?.pages || [],
+        elements: data?.elements || [],
         assets: (data?.elements || []).reduce((assets, element) => {
             if (element.assetId && data?.assets?.[element.assetId]) {
                 assets[element.assetId] = data.assets[element.assetId];
