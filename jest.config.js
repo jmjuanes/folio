77eb5157eff9e41e811e6bfc6e635e341b9dbfda
Josemi <josemijuanes@gmail.com>
2026-07-05 16:41:10 +0200

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import("jest").Config} */
 export default {
     testEnvironment: "jsdom",
+    setupFilesAfterEnv: [
+        "@testing-library/jest-dom",
+    ],
     transform: {
         "^.+\\.(j|t)sx?$": "babel-jest",
     },
